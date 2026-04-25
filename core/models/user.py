32c7de286bec1678c3702a12bbaa607f8d87235b
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-25 13:18:53 +0300

    Update user.py

--- core/models/user.py
+++ core/models/user.py
@@ -1,1 +1,14 @@
+from core import db
+from datetime import datetime
+from flask_login import UserMixin
 
+class User(db.Model, UserMixin):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    role = db.Column(db.String(20), default='admin') # 'admin' للتحكم المطلق
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Admin: {self.username}>'
