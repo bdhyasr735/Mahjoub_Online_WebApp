99a6ff1b27420ec66b9209e4e73d795ae2930728
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-27 15:01:43 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-# إجبار السيرفر على رؤية المجلد الحالي كحزمة
+# إجبار بايثون على إضافة المسار الحالي للمشروع
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 from core import create_app
@@ -9,6 +9,6 @@ from core import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    # تشغيل متوافق مع Railway
-    port = int(os.environ.get("PORT", 5000))
+    # التوافق مع بورت Railway
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
