2141350a3f05b12256a8118f1c466229d74a0c62
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-27 14:21:23 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
+import os
 from core import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    # بايثون سيتعرف على بورت Railway آلياً
+    # الحصول على البورت من Railway أو استخدام 5000 كافتراضي
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
