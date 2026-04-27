a17c72eceb916e145b1ae50a0df2057095d6fb05
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-27 14:20:58 +0300

    Update __init__.py

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,9 +1,7 @@
 from core import db
 
-# استيراد الموديلات من الملفات الفرعية داخل المجلد
 from .user import User
 from .supplier import Supplier
 from .product import Product
 
-# جعل الموديلات متاحة للاستيراد المباشر
 __all__ = ['User', 'Supplier', 'Product']
