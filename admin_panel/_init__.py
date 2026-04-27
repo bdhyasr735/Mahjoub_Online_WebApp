734f25adca953acef6fa1e481e92c7c64d242ee7
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-27 23:07:41 +0300

    Refactor comments in admin panel initialization

--- admin_panel/_init__.py
+++ admin_panel/_init__.py
@@ -1,12 +1,12 @@
 from flask import Blueprint
 
-# تعريف البوابة البرمجية للإدارة
+# 1. تعريف البلوبرينت السيادي للإدارة
+# قمنا بتحديد template_folder لكي يقرأ ملفات الـ HTML من داخل مجلد templates الخاص به
 admin_bp = Blueprint(
     'admin_panel', 
     __name__, 
-    template_folder='templates',
-    static_folder='static'
+    template_folder='templates'
 )
 
-# استيراد المسارات لربطها بالبوابة
+# 2. استيراد الروابط (Routes) بعد تعريف البلوبرينت لمنع الخطأ الدوري (Circular Import)
 from . import routes
