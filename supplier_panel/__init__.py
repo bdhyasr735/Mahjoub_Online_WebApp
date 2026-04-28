8de3052428ada8f0b75c5f1bcba0399e326a5e43
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-28 19:13:53 +0300

    Update __init__.py

--- supplier_panel/__init__.py
+++ supplier_panel/__init__.py
@@ -1,7 +1,5 @@
 from flask import Blueprint
 
-# تعريف البلوبرنت
 supplier_bp = Blueprint('supplier_panel', __name__, template_folder='templates')
 
-# استيراد المسارات لربطها بالبلوبرنت
 from . import routes
