9d2a9887e39a664db93fa5f9fc63f80068e76e6d
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-28 19:53:53 +0300

    Update user.py

--- core/models/user.py
+++ core/models/user.py
@@ -1,12 +1,18 @@
 from core import db
 from flask_login import UserMixin
-from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-    role = db.Column(db.String(20), default='supplier') # 'admin' or 'supplier'
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    password_hash = db.Column(db.String(120), nullable=False)
     
-    supplier_profile = db.relationship('Supplier', backref='user_account', uselist=False)
+    # تأكد من وجود هذين السطرين تحديداً
+    role = db.Column(db.String(20), default='user') # 'admin' or 'supplier'
+    status = db.Column(db.String(20), default='pending') # 'approved' or 'pending'
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
