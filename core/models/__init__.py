53fe80e93a7f60062c5c5210b942768fb097b74d
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-29 16:15:51 +0300

    Update __init__.py

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,6 +1,3 @@
-from core.models.user import User
-from core.models.product import Product
-from core.models.supplier import Supplier
-
-# هذا السطر يسهل عملية استدعاء db.create_all() من ملف النواة الرئيسي
-__all__ = ['User', 'Product', 'Supplier']
+from .user import User
+from .product import Product
+from .supplier import Supplier
