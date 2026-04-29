fdadd22da64ef96b79f99fa173f08fd4ef3abb04
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-29 19:11:52 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
+import os
 from core import create_app
 
-# إنشاء نسخة التطبيق من "العاصمة" core
+# إنشاء نسخة التطبيق
 app = create_app()
 
 if __name__ == '__main__':
-    # تشغيل السيرفر
-    # ملاحظة: عند الرفع على Railway، السيرفر يستخدم Gunicorn ويقرأ من هذا الملف
-    app.run(debug=True)
+    # الحصول على المنفذ من بيئة Render أو استخدام 10000 كافتراضي
+    port = int(os.environ.get("PORT", 10000))
+    
+    # تشغيل السيرفر مع تحديد المنفذ والمضيف (Host) ليكون متاحاً للإنترنت
+    app.run(host='0.0.0.0', port=port, debug=False)
