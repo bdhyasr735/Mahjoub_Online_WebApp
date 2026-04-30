112c12742dec89aef05f7d7f621dba7ceb4c1adc
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-30 23:04:56 +0300

    Update _init__.py

--- admin_panel/_init__.py
+++ admin_panel/_init__.py
@@ -1,10 +1,7 @@
 from flask import Blueprint
 
-# تعريف البلوبرنت بوضوح
+# تعريف البلوبرنت
 admin_panel = Blueprint('admin_panel', __name__, template_folder='templates')
 
-# استيراد المسارات لربطها بالبلوبرنت
+# استيراد المسارات بعد تعريف البلوبرنت لتجنب التداخل
 from . import routes
-
-# هذا السطر يضمن تصدير الكائن بشكل صحيح للسيرفر
-__all__ = ['admin_panel']
