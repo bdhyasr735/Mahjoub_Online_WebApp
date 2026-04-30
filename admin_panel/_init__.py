c9d853284b2365fca92d450deff92a0cf96a4413
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-30 22:13:05 +0300

    Update _init__.py

--- admin_panel/_init__.py
+++ admin_panel/_init__.py
@@ -1,13 +1,11 @@
 from flask import Blueprint
 
-# 1. تعريف البلوبرنت الخاص ببرج الرقابة المركزية
-# نحدد template_folder ليتعرف النظام على المجلد الذي يحتوي على login.html و dashboard.html
+# تعريف البلوبرنت وتحديد مجلد القوالب
 admin_panel = Blueprint(
     'admin_panel', 
     __name__, 
     template_folder='templates'
 )
 
-# 2. استيراد المسارات (Routes) لربطها بالبلوبرنت
-# نضع الاستيراد هنا في الأسفل لتجنب مشكلة الدوران البرمجي (Circular Import)
+# استيراد المسارات بعد تعريف البلوبرنت لتجنب الخطأ الدائري
 from . import routes
