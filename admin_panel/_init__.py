d74677fc976ae83e47c739798a035253f9bd323b
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-30 23:13:35 +0300

    Update _init__.py

--- admin_panel/_init__.py
+++ admin_panel/_init__.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
 
-# تعريف البلوبرنت
-admin_panel = Blueprint('admin_panel', __name__, template_folder='templates')
+# قمنا بتغيير اسم المتغير البرمجي إلى admin_blueprint 
+# لكي لا يختلط الأمر على بايثون بين اسم "المجلد" واسم "الكائن"
+admin_blueprint = Blueprint('admin_panel', __name__, template_folder='templates')
 
-# استيراد المسارات بعد تعريف البلوبرنت لتجنب التداخل
+# استدعاء المسارات لربطها بالبلوبرنت المسمى حديثاً
 from . import routes
