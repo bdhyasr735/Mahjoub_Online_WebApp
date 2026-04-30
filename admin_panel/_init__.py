600fa0d03e5668b087c86cfb4bd65da9963d578b
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-30 22:17:50 +0300

    Update _init__.py

--- admin_panel/_init__.py
+++ admin_panel/_init__.py
@@ -1,11 +1,7 @@
 from flask import Blueprint
 
-# تعريف البلوبرنت وتحديد مجلد القوالب
-admin_panel = Blueprint(
-    'admin_panel', 
-    __name__, 
-    template_folder='templates'
-)
+# تعريف الكائن أولاً
+admin_panel = Blueprint('admin_panel', __name__, template_folder='templates')
 
-# استيراد المسارات بعد تعريف البلوبرنت لتجنب الخطأ الدائري
+# الاستيراد في الأسفل لتجنب التعليق الدائري
 from . import routes
