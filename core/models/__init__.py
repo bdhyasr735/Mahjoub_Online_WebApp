3b9bee413faa8ae4e8ed409974005e3030e3b741
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-30 20:50:10 +0300

    Update __init__.py

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,3 +1,8 @@
-from .user import User
-from .product import Product
-from .supplier import Supplier
+from core import db
+from core.models.user import User
+from core.models.supplier import Supplier
+from core.models.product import Product
+
+# تعريف القائمة التي تسمح بالاستيراد النظيف من خارج المجلد
+# هذا يمنع تكرار أسطر الاستيراد (Import redundancy) في الـ routes
+__all__ = ['User', 'Supplier', 'Product']
