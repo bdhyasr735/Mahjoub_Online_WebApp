ed90830ca3bcd34ba3b4e55ac16d512f599f28bc
alimohm <69640611+alimohm@users.noreply.github.com>
2026-05-12 15:10:37 +0300

    Delete apps/ admin_dashboard directory

--- apps/ admin_dashboard/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-
-# تعريف البلوبرنت الخاص بالإدارة
-admin_bp = Blueprint('admin', __name__, template_folder='templates')
-
-@admin_bp.route('/dashboard')
-def dashboard():
-    # استدعاء ملف المحتوى من المسار: templates/admin/dashboard_content.html
-    return render_template('admin/dashboard_content.html')
