84f3d7708532d5a46f91f519c58ef3819a1849f2
alimohm <69640611+alimohm@users.noreply.github.com>
2026-05-13 20:20:13 +0300

    Update __init__.py

--- apps/admin_dashboard/__init__.py
+++ apps/admin_dashboard/__init__.py
@@ -1,1 +1,7 @@
+from flask import Blueprint
 
+# إنشاء البلوبرينت أولاً
+admin_dashboard = Blueprint('admin_dashboard', __name__, template_folder='templates')
+
+# استيراد المسارات في نهاية الملف لمنع التضارب (Circular Import)
+from . import routes
