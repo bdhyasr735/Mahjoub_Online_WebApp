7213a28e8bd8a6d0fee3b3e24005642d0293a6ad
alimohm <69640611+alimohm@users.noreply.github.com>
2026-05-13 17:18:12 +0300

    Update routes.py

--- apps/admin_dashboard/routes.py
+++ apps/admin_dashboard/routes.py
@@ -1,15 +1,9 @@
 from flask import Blueprint, render_template
 
-# تأكد أن الاسم هنا هو admin_dashboard كما سجلناه في __init__.py
 admin_dashboard = Blueprint('admin_dashboard', __name__, template_folder='templates')
 
 @admin_dashboard.route('/')
 @admin_dashboard.route('/dashboard')
 def dashboard():
-    # هذا المسار سيفتح لوحة التحكم المركزية
-    return render_template('admin/admin_base.html')
-
-@admin_dashboard.route('/suppliers')
-def list_suppliers():
-    # هذا المسار لعرض جدول الموردين (السجل)
-    return render_template('admin/list_suppliers.html')
+    # هنا نستدعي ملف المحتوى الذي سيرث الهيكل تلقائياً
+    return render_template('admin/dashboard_content.html')
