720fbf0b21b2e9866df423ed46beb9dba7bf2646
alimohm <69640611+alimohm@users.noreply.github.com>
2026-05-14 12:58:27 +0300

    Update __init__.py

--- apps/add_supplier/__init__.py
+++ apps/add_supplier/__init__.py
@@ -1,10 +1,15 @@
+# coding: utf-8
 from flask import Blueprint
 
-# تعريف البلوبرينت باسم مستقل
-add_supplier_bp = Blueprint(
-    'admin_suppliers', # الاسم البرمجي للاستدعاء
+# تعريف البلوبرينت (Blueprint) الخاص بتطبيق إضافة الموردين
+# ملاحظة: تم اختيار 'admin_suppliers' ليتوافق مع استدعاءات الـ URL في المنظومة
+admin_suppliers = Blueprint(
+    'admin_suppliers', 
     __name__,
-    template_folder='templates'
+    template_folder='templates',
+    static_folder='static'
 )
 
+# استيراد المسارات (Routes) لربطها بالبلوبرينت
+# يتم الاستيراد في الأسفل لتجنب مشكلة الاستيراد الدائري (Circular Import)
 from . import routes
