fff4e8e4022c7b88612238ac775a0b1ff4f10faa
Ali Mahjoub <alyasserandsonsfoodstuffs@gmail.com>
2026-05-24 17:49:26 +0300

    Update __init__.py

--- apps/wallet/__init__.py
+++ apps/wallet/__init__.py
@@ -1,14 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-📂 apps/wallet/__init__.py
-ملف تهيئة حزمة إدارة المحافظ والتسويات المادية الشاملة
-منصة محجوب أونلاين - سوقك الذكي (2026)
-"""
+# coding: utf-8
+from flask import Blueprint
 
-# استدعاء الـ Blueprint المحدث ومحرك التسويات من الملف الحوكمي الجديد
-from apps.wallet.approvals_and_settlements import wallet_blueprint
-# استدعاء النماذج لضمان تسجيل الجداول (الأب والابن) ضمن سياق قاعدة البيانات
-from apps.wallet import models
-
-# جعل الـ Blueprint متاحاً بشكل مباشر عند استدعاء الحزمة من التطبيق الرئيسي
-__all__ = ['wallet_blueprint']
+# استيراد البلوبرينت من ملف الـ routes الفعلي في نفس المجلد
+from .routes import wallet_blueprint
