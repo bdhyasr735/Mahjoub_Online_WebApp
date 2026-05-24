0f56b531d5b0c460f1fcdfcd8668af061b409163
Ali Mahjoub <alyasserandsonsfoodstuffs@gmail.com>
2026-05-24 19:28:39 +0300

    Update __init__.py

--- apps/wallet/__init__.py
+++ apps/wallet/__init__.py
@@ -1,13 +1,8 @@
 # coding: utf-8
 from flask import Blueprint
 
-# تعريف البلوبرينت مع تحديد مجلد القوالب (templates)
-# هذا التحديد يضمن أن Flask سيبحث عن ملفات HTML داخل المجلد الفرعي
 wallet_blueprint = Blueprint(
     'wallet', 
     __name__, 
-    template_folder='templates'
+    template_folder='templates'  # يخبر Flask بالبحث داخل مجلد templates الملازم للملف
 )
-
-# ملاحظة: لا تستورد routes هنا لمنع الـ Circular Import.
-# يتم استيراد الـ routes وتسجيل البلوبرينت في ملف apps/__init__.py الرئيسي.
