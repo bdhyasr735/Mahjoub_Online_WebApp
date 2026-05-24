2a2fcac44dd0c5e07d3f6cc0763413f9b6e95bf9
Ali Mahjoub <alyasserandsonsfoodstuffs@gmail.com>
2026-05-24 19:35:24 +0300

    Update __init__.py

--- apps/wallet/__init__.py
+++ apps/wallet/__init__.py
@@ -1,8 +1,11 @@
-# coding: utf-8
+import os
 from flask import Blueprint
+
+# تحديد المسار المطلق لمجلد القوالب لضمان استقلاليته عن أي شيء آخر
+template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 
 wallet_blueprint = Blueprint(
     'wallet', 
     __name__, 
-    template_folder='templates'  # يخبر Flask بالبحث داخل مجلد templates الملازم للملف
+    template_folder=template_dir  # المسار المطلق يضمن عدم ضياع Flask
 )
