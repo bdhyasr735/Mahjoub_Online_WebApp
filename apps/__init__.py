7b5965292bf4fbe11b7577afbfdf4ad43bbb68f4
Ali Mahjoub <alyasserandsonsfoodstuffs@gmail.com>
2026-05-24 17:51:46 +0300

    Update __init__.py

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,4 +1,4 @@
-# coding: utf-8
+# apps/__init__.py
 from flask import Flask
 from apps.extensions import db, login_manager
 from config import Config
@@ -15,15 +15,13 @@ def create_app():
     login_manager.login_view = 'auth_portal.login' 
 
     with app.app_context():
-        # تسجيل المحافظ (يستورد من __init__ الخاص بـ wallet الذي صححناه أعلاه)
-        from apps.wallet import wallet_blueprint
+        # تسجيل البلوبرينتس (تأكد من صحة مسارات الاستيراد)
+        from apps.wallet.routes import wallet_blueprint
         app.register_blueprint(wallet_blueprint, url_prefix='/wallet')
 
-        # تسجيل المصادقة
         from apps.auth_portal.routes import auth_blueprint
         app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
-        # تسجيل لوحة التحكم
         from apps.admin_dashboard.routes import admin_dashboard
         app.register_blueprint(admin_dashboard)
 
