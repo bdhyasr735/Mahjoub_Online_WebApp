faf2e184ca4c29a70d5072945e510fcf467f8ce3
Ali Mahjoub <alyasserandsonsfoodstuffs@gmail.com>
2026-05-25 12:10:26 +0300

    Update __init__.py

--- apps/models/__init__.py
+++ apps/models/__init__.py
@@ -4,7 +4,7 @@
 from apps.models.admin_db import AdminUser
 from apps.models.supplier_db import Supplier
 # إضافة الموديل الجديد هنا:
-from apps.models.settlements_db import AdminSettlement 
+from apps.models.settlements_db import AdminSettlement
 
 # يفضل أيضاً إضافة موديل المحفظة إذا كان موجوداً في ملف مستقل، 
 # تأكد من استيراد كل شيء تحتاجه هنا لتجنب أي مشاكل عند الاستيراد.
