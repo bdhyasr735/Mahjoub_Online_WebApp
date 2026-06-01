3f0b6b71f485b41593ed82a25fafe36f4596a704
alimohm <69640611+alimohm@users.noreply.github.com>
2026-06-01 19:59:35 +0300

    ط

--- api/index.py
+++ api/index.py
@@ -1,13 +1,10 @@
 import sys
 import os
 
-# إضافة المسار الحالي للمشروع إلى مسار البحث عن المكتبات
-# هذا يجعل مجلد 'apps' مرئياً كجزء من بيئة بايثون
-current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(current_dir)
-sys.path.append(parent_dir)
+# إضافة المجلد الحالي للمسارات لضمان رؤية المجلدات الفرعية
+sys.path.append(os.getcwd())
 
-# استيراد التطبيق من ملف run.py الموجود في المجلد الرئيسي
+# استيراد 'app' من ملف run.py
 from run import app
 
-# Vercel يحدد هذا المتغير كـ Entrypoint
+# Vercel يحتاج كائن يسمى 'app'
