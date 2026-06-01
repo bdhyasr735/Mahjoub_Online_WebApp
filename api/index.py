39c3d45b5ae2a37c8e8b9c20733d665a6c1bf237
alimohm <69640611+alimohm@users.noreply.github.com>
2026-06-01 20:53:13 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,13 +1,3 @@
-import sys
-import os
+from run import app
 
-# إضافة المجلد الحالي للمسارات لضمان رؤية مجلد 'apps'
-sys.path.append(os.getcwd())
-
-# استيراد دالة المصنع من المجلد الخاص بك
-from apps import create_app
-
-# إنشاء التطبيق (Vercel يحتاج كائن 'app' في هذا الملف)
-app = create_app()
-
-# الآن Vercel سيجد كائن 'app' وسيقوم بتشغيل المنصة بنجاح
+# Vercel سيقرأ هذا الملف، ويقوم بتصدير 'app' ليتمكن من تشغيل الموقع
