fc22e14aa71fd254afb4574049245c8af0622d0d
alimohm <69640611+alimohm@users.noreply.github.com>
2026-06-01 20:34:20 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,1 @@
-import sys
-import os
-
-# إضافة المجلد الحالي للمسارات لضمان رؤية المجلدات الفرعية
-sys.path.append(os.getcwd())
-
-# استيراد 'app' من ملف run.py
 from run import app
-
-# Vercel يحتاج كائن يسمى 'app'
