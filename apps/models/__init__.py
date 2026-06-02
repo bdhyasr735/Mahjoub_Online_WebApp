8a1c8718a5d49e04fa2a24d0eacebbb782931048
alimohm <69640611+alimohm@users.noreply.github.com>
2026-06-03 00:18:48 +0300

    Update __init__.py

--- apps/models/__init__.py
+++ apps/models/__init__.py
@@ -1,20 +1,5 @@
-# coding: utf-8
-# 📂 apps/models/__init__.py
-# هذا الملف مسؤول عن تصدير جميع جداول قاعدة البيانات (Models) 
-# ليتسنى لـ SQLAlchemy و Flask-Migrate التعرف عليها.
-
-from apps.models.admin_db import AdminUser
-from apps.models.supplier_db import Supplier
-from apps.models.wallet_db import Wallet, WalletTransaction
-from apps.models.settlements_db import AdminSettlement
-from apps.models.statement_db import SupplierStatement
-
-# قائمة التصدير الرسمية لتنظيم الوصول للنماذج
-__all__ = [
-    'AdminUser',
-    'Supplier',
-    'Wallet',
-    'WalletTransaction',
-    'AdminSettlement',
-    'SupplierStatement'
-]
+from .admin_db import AdminUser
+from .supplier_db import Supplier
+from .wallet_db import SupplierWallet, WalletTransaction
+from .settlements_db import AdminSettlement
+from .statement_db import SupplierStatement
