f9a4979c82978aefb858c8cf58b1fbd036dbdf20
alimohm <69640611+alimohm@users.noreply.github.com>
2026-06-02 21:25:07 +0300

    Update __init__.py

--- apps/models/__init__.py
+++ apps/models/__init__.py
@@ -1,14 +1,10 @@
 # coding: utf-8
-# 📂 apps/models/__init__.py
-# حزمة الإشهار المركزية لنماذج قاعدة البيانات - منصة محجوب أونلاين 2026
-
 from apps.models.admin_db import AdminUser
 from apps.models.supplier_db import Supplier
 from apps.models.wallet_db import Wallet, WalletTransaction
 from apps.models.settlements_db import AdminSettlement
 from apps.models.statement_db import SupplierStatement
 
-# القائمة السيادية الحاكمة للاستيراد الخارجي النظيف لمنع تداخل الحزم
 __all__ = [
     'AdminUser',
     'Supplier',
