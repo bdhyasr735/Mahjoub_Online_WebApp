25b9ac370f2bea6322a3f4ec2062f4743734952c
alimohm <69640611+alimohm@users.noreply.github.com>
2026-06-03 20:39:09 +0300

    Update __init__.py

--- apps/auth_portal/__init__.py
+++ apps/auth_portal/__init__.py
@@ -1,6 +1,15 @@
+# coding: utf-8
+# 📂 apps/auth_portal/__init__.py - تعريف بوابة الدخول
+
 from flask import Blueprint
 
-# هذا المتغير auth_portal هو ما يبحث عنه ملف routes.py
-auth_portal = Blueprint('auth_portal', __name__, template_folder='templates')
+# تعريف الـ Blueprint الخاص ببوابة الدخول
+auth_portal = Blueprint(
+    'auth_portal', 
+    __name__, 
+    template_folder='templates',
+    static_folder='static'
+)
 
+# استيراد المسارات لضمان تسجيلها عند تحميل الـ Blueprint
 from . import routes
