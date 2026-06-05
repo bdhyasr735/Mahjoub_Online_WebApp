4748b6a55fcec8f40d0d5ee43f09e2680c767a79
alimohm <69640611+alimohm@users.noreply.github.com>
2026-06-05 20:51:37 +0300

    Update env.py

--- migrations/env.py
+++ migrations/env.py
@@ -1,1 +1,12 @@
+from apps import create_app # تأكد أن هذا المسار يشير لمجلد مشروعك الرئيسي
+from apps.extensions import db
 
+config = context.config
+app = create_app()
+
+# هذا الجزء يربط Alembic بقاعدة بيانات Flask
+target_metadata = db.metadata
+
+def run_migrations_online():
+    connectable = db.engine
+    # ... بقية الكود التلقائي
