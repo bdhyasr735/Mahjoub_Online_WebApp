4444726194a9471820e859f62fe4d9193cf726d5
alimohm <69640611+alimohm@users.noreply.github.com>
2026-06-06 17:28:28 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
-# 📂 run.py
+# 📂 run.py - النسخة الأكثر استقراراً
 from apps import create_app
 import os
 
+# تهيئة التطبيق
 app = create_app()
 
 if __name__ == "__main__":
+    # هذا التشغيل المحلي فقط
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
