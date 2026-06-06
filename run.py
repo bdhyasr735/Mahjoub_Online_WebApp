96458bb519e51bc4e07cc1d33905e7c21b90c7e7
alimohm <69640611+alimohm@users.noreply.github.com>
2026-06-06 19:10:00 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
-import os
+# coding: utf-8
+# 📂 run.py (موجود في المجلد الرئيسي بجانب config.py)
+
 from apps import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
