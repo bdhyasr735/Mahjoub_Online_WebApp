f67bd54f27d7c85ea59dc3448dd939fc15113d74
alimohm <69640611+alimohm@users.noreply.github.com>
2026-06-09 20:48:53 +0300

    Update __init__.py

--- apps/wallet/__init__.py
+++ apps/wallet/__init__.py
@@ -1,11 +1,2 @@
 # 📂 apps/wallet/__init__.py
-from flask import Blueprint
-
-# تعريف الـ Blueprint للمحفظة
-# 'wallet_app' هو الاسم الذي سنستخدمه في تسجيل الـ Blueprint في __init__.py الرئيسي
-# template_folder='templates' يحدد أن القوالب الخاصة بهذا الجزء موجودة في مجلد templates الفرعي
-wallet_app = Blueprint('wallet_app', __name__, template_folder='templates')
-
-# استيراد ملف المسارات (Routes) في نهاية الملف 
-# هذا ضروري جداً لكي يتعرف Flask على المسارات الموجودة في routes.py
-from apps.wallet import routes
+from apps.wallet.routes import wallet_app
