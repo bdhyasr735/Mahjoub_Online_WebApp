084943f71ad638d7358f26c5ae360f3b263edcec
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-06-17 03:35:28 +0900

    Update __init__.py

--- apps/models/__init__.py
+++ apps/models/__init__.py
@@ -3,22 +3,7 @@
 
 from .admin_db import AdminUser
 from .bridge_db import BridgeLog
-from .financial_db import FinancialLog
-from .orders_db import ProcessedOrder
-from .settlements_db import Settlement
-from .supplier_db import Supplier
-from .vault_db import AdminVault, VaultTransaction
-from .wallet_db import SupplierWallet, WalletTransaction
-
-__all__ = [
-    'AdminUser',
-    'BridgeLog',# coding: utf-8
-# 📂 apps/models/__init__.py
-
-from .admin_db import AdminUser
-from .bridge_db import BridgeLog
-from .financial_db import FinancialLog
-from .orders_db import ProcessedOrder
+from .financial_db import FinancialLog, ExchangeRate
 from .settlements_db import Settlement
 from .supplier_db import Supplier
 from .vault_db import AdminVault, VaultTransaction
@@ -28,16 +13,7 @@ __all__ = [
     'AdminUser',
     'BridgeLog',
     'FinancialLog',
-    'ProcessedOrder',
-    'Settlement',
-    'Supplier',
-    'AdminVault',
-    'VaultTransaction',
-    'SupplierWallet',
-    'WalletTransaction'
-]
-    'FinancialLog',
-    'ProcessedOrder',
+    'ExchangeRate',
     'Settlement',
     'Supplier',
     'AdminVault',
