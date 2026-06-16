dba0ba331ee15e0ea3bfa8084fa7ff01bd892378
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-06-16 14:04:40 +0300

    Update __init__.py

--- apps/orders/__init__.py
+++ apps/orders/__init__.py
@@ -1,7 +1,3 @@
 # 📂 apps/orders/__init__.py
-
-# هذا الملف يجعل مجلد 'orders' بمثابة Python Package
-# يمكنك تركه فارغاً، أو استيراد الـ Blueprint مباشرة لتسهيل الوصول إليه
-from apps.orders.routes import orders_bp
-
-# لا حاجة لأي شيء آخر هنا حالياً
+# تم إفراغ هذا الملف لمنع Circular Imports.
+# لا تستورد الـ Blueprint هنا.
