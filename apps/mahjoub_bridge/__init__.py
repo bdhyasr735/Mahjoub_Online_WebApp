96f0b1fb35ede45ebf6cfe76b2aa8fcba5e0983c
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-06-16 13:54:59 +0300

    Update __init__.py

--- apps/mahjoub_bridge/__init__.py
+++ apps/mahjoub_bridge/__init__.py
@@ -1,8 +1,6 @@
-# coding: utf-8
+# 📂 apps/mahjoub_bridge/__init__.py
 from flask import Blueprint
 
-# تعريف الجسر كـ Blueprint
+# تعريف الـ Blueprint
+# ملاحظة: تأكد أن الاسم يطابق ما تستخدمه في routes.py
 mahjoub_bridge = Blueprint('mahjoub_bridge', __name__, template_folder='templates')
-
-# سنقوم لاحقاً باستيراد المسارات هنا
-from apps.mahjoub_bridge import routes
