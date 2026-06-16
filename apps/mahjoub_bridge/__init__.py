1e53ec5308aff238573ad6ecc8fab1c8cb2ef38e
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-06-17 03:29:12 +0900

    Delete apps/mahjoub_bridge directory

--- apps/mahjoub_bridge/__init__.py
+++
@@ -1,6 +0,0 @@
-# 📂 apps/mahjoub_bridge/__init__.py
-from flask import Blueprint
-
-# تعريف الـ Blueprint
-# ملاحظة: تأكد أن الاسم يطابق ما تستخدمه في routes.py
-mahjoub_bridge = Blueprint('mahjoub_bridge', __name__, template_folder='templates')
