f4a95d39eec967dcc92d62cad08e26870b695b7b
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-06-17 04:08:20 +0900

    Update __init__.py

--- apps/models/__init__.py
+++ apps/models/__init__.py
@@ -1,1 +1,9 @@
+# 📂 apps/models/__init__.py
+from .admin_db import AdminUser
+from .supplier_db import Supplier
+from .wallet_db import SupplierWallet
+from .financial_db import ExchangeRate
+from .vault_db import AdminVault
+from .orders_db import ProcessedOrder
 
+# تأكد من عدم وجود أي سطر يحتوي على 'settlement' أو أي موديل حذفته.
