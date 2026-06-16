a3132bb0c3f2f6ef3da2488b4a45a94be300ff33
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-06-17 03:24:34 +0900

    Update admin_db.py

--- apps/models/admin_db.py
+++ apps/models/admin_db.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-# 📂 apps/models/admin_db.py
+# 📂 apps/models/admin_db.py - نظام الهوية والتشفير السيادي
 
 import os
 from apps.extensions import db
@@ -19,33 +19,39 @@ class AdminUser(db.Model, UserMixin):
     is_active = db.Column(db.Boolean, default=True)
 
     def _get_encryption_key(self):
-        # الاعتماد على الإعدادات المركزية
-        return current_app.config.get('ENCRYPTION_KEY') or os.environ.get('ENCRYPTION_KEY')
+        """جلب المفتاح من الإعدادات المركزية مع قيمة افتراضية آمنة للإقلاع"""
+        try:
+            return current_app.config.get('ENCRYPTION_KEY') or os.environ.get('ENCRYPTION_KEY', 'w1Kk9P7zY5mZg4tE8Lp2nJvR6cXsA9qB0xU3jH5oI8Vq=')
+        except:
+            return os.environ.get('ENCRYPTION_KEY', 'w1Kk9P7zY5mZg4tE8Lp2nJvR6cXsA9qB0xU3jH5oI8Vq=')
 
     @property
     def phone_number(self):
+        """فك تشفير رقم الهاتف السيادي"""
         if self._phone_number_enc:
             try:
                 key = self._get_encryption_key()
-                if key:
-                    cipher = Fernet(key.encode())
-                    return cipher.decrypt(self._phone_number_enc.encode()).decode()
+                cipher = Fernet(key.encode())
+                return cipher.decrypt(self._phone_number_enc.encode()).decode()
             except:
                 return "Error"
         return ""
     
     @phone_number.setter
     def phone_number(self, value):
+        """تشفير رقم الهاتف قبل الحفظ في قاعدة البيانات"""
         if value:
             key = self._get_encryption_key()
-            if key:
-                cipher = Fernet(key.encode())
-                self._phone_number_enc = cipher.encrypt(str(value).encode()).decode()
-            else:
-                self._phone_number_enc = str(value)
+            cipher = Fernet(key.encode())
+            self._phone_number_enc = cipher.encrypt(str(value).encode()).decode()
+        else:
+            self._phone_number_enc = None
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return f'<AdminUser {self.username}>'
