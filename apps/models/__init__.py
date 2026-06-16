a84f03ac159ea9643f36393db88621f392a2cfc5
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-06-17 04:05:52 +0900

    Update __init__.py

--- apps/models/__init__.py
+++ apps/models/__init__.py
@@ -1,19 +1,1 @@
-# coding: utf-8
-# 📂 apps/models/__init__.py - التنسيق المعتمد والمطهر
 
-from .admin_db import AdminUser
-from .supplier_db import Supplier
-from .wallet_db import SupplierWallet
-from .financial_db import ExchangeRate
-from .vault_db import AdminVault
-
-# لا تقم باستيراد ProcessedOrder أو Settlement أو أي موديل آخر قمت بحذفه فعلياً من مجلد models
-# هذا هو الهيكل الذي يتوافق مع ما قمت بحذفه:
-
-__all__ = [
-    'AdminUser',
-    'Supplier',
-    'SupplierWallet',
-    'ExchangeRate',
-    'AdminVault'
-]
