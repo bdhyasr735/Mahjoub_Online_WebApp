825a83476614e20bd08772de06d6ede0f4d6c5a8
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-06-21 19:25:57 +0300

    Update orders_db.py

--- apps/models/orders_db.py
+++ apps/models/orders_db.py
@@ -1,4 +1,6 @@
 # coding: utf-8
+# 📂 apps/models/order_db.py
+
 from apps.extensions import db
 from apps.utils.security import AESCipher
 
@@ -6,21 +8,23 @@ class ProcessedOrder(db.Model):
     __tablename__ = 'processed_orders'
 
     # --- الأعمدة الأساسية ---
-    id = db.Column(db.String(100), primary_key=True) # هذا سيكون الـ qid
+    id = db.Column(db.String(100), primary_key=True)  # QID
     order_id = db.Column(db.String(50))
     order_status = db.Column(db.String(50))
-    financial_status = db.Column(db.String(50))      # حقل جديد
-    fulfillment_status = db.Column(db.String(50))    # حقل جديد
+    financial_status = db.Column(db.String(50))
+    fulfillment_status = db.Column(db.String(50))
     shipping_city = db.Column(db.String(100))
     shipping_street = db.Column(db.String(200))
+    # مفتاح ربط مع المورد
+    supplier_id = db.Column(db.Integer, db.ForeignKey('suppliers.id'), nullable=True)
 
-    # --- الحقول المشفرة ---
+    # --- الحقول المشفرة (تخزن كـ String مشفر في DB) ---
     _total_price = db.Column('total_price', db.String(255))
     _customer_name = db.Column('customer_name', db.String(255))
     _customer_phone = db.Column('customer_phone', db.String(255))
     _customer_email = db.Column('customer_email', db.String(255))
 
-    # --- Property للحقول المشفرة ---
+    # --- Properties للتشفير وفك التشفير ---
     
     @property
     def total_price(self):
@@ -33,7 +37,7 @@ class ProcessedOrder(db.Model):
 
     @property
     def customer_name(self):
-        return AESCipher.decrypt(self._customer_name)
+        return AESCipher.decrypt(self._customer_name) or ""
 
     @customer_name.setter
     def customer_name(self, value):
@@ -41,15 +45,15 @@ class ProcessedOrder(db.Model):
 
     @property
     def customer_phone(self):
-        return AESCipher.decrypt(self._customer_phone)
+        return AESCipher.decrypt(self._customer_phone) or ""
 
-    @customer_phone.setter # تم تصحيح الخطأ هنا
+    @customer_phone.setter
     def customer_phone(self, value):
         self._customer_phone = AESCipher.encrypt(str(value))
 
     @property
     def customer_email(self):
-        return AESCipher.decrypt(self._customer_email)
+        return AESCipher.decrypt(self._customer_email) or ""
 
     @customer_email.setter
     def customer_email(self, value):
@@ -63,7 +67,6 @@ class OrderItem(db.Model):
     order_id = db.Column(db.String(100), db.ForeignKey('processed_orders.id'))
     product_title = db.Column(db.String(200))
     quantity = db.Column(db.Integer)
-
     _price = db.Column('price', db.String(255))
 
     @property
