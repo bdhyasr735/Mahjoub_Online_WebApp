0955161946cbcc42c690bc6a0c8ee1a93bd02cac
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-06-21 21:08:04 +0300

    Update __init__.py

--- apps/vendor_dashboard/__init__.py
+++ apps/vendor_dashboard/__init__.py
@@ -1,1 +1,5 @@
+# 📂 apps/vendor_dashboard/__init__.py
 
+# هذا الملف يجعل المجلد حزمة بايثون (Package)
+# يمكنك تركه فارغاً، أو استيراد الـ Blueprint هنا لتسهيل الوصول إليه
+from apps.vendor_dashboard.routes import dashboard_bp
