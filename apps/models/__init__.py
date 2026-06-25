a4fffbe5d2624ab3245cb9c1cb72628bf6edb72b
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-06-26 05:44:06 +0900

    Update __init__.py

--- apps/models/__init__.py
+++ apps/models/__init__.py
@@ -1,29 +1,31 @@
 # coding: utf-8
 # 📂 apps/models/__init__.py
 
+# نقوم باستيراد النماذج لضمان تهيئتها بواسطة SQLAlchemy
+# تم ترتيب الاستيراد لضمان أن الجداول الأساسية (مثل Supplier) 
+# معرفة قبل الجداول المرتبطة بها (مثل SupplierStaff, Wallet)
+
 from .admin_db import AdminUser
 from .admin_staff_db import AdminStaff
+from .supplier_db import Supplier
+from .supplier_staff_db import SupplierStaff
+from .supplier_profile_db import SupplierProfile
+from .wallet_db import SupplierWallet
+from .orders_db import Order
 from .financials_db import OrderFinancial
 from .marketers_db import Marketer
-from .orders_db import Order
-from .supplier_db import Supplier
-from .supplier_profile_db import SupplierProfile
-from .supplier_staff_db import SupplierStaff
 from .sync_log import SyncLog
-from .wallet_db import SupplierWallet
 
-# القائمة الموحدة للتصدير (Export) 
-# هذا يسهل عملية الاستيراد في الملفات الأخرى مثل:
-# from apps.models import Supplier, SupplierWallet, Order
+# القائمة الموحدة للتصدير
 __all__ = [
     'AdminUser',
     'AdminStaff',
+    'Supplier',
+    'SupplierStaff',
+    'SupplierProfile',
+    'SupplierWallet',
+    'Order',
     'OrderFinancial',
     'Marketer',
-    'Order',
-    'Supplier',
-    'SupplierProfile',
-    'SupplierStaff',
-    'SyncLog',
-    'SupplierWallet'
+    'SyncLog'
 ]
