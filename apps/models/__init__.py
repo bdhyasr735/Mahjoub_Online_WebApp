b4b0dd87604e910fb9d2e5cdd60c40de3f709fb2
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-06-26 00:08:56 +0900

    Update __init__.py

--- apps/models/__init__.py
+++ apps/models/__init__.py
@@ -10,7 +10,26 @@ from .otp_db import OTPVerification
 from .orders_db import Order
 from .supplier_db import Supplier
 from .supplier_profile_db import SupplierProfile
+from .supplier_staff_db import SupplierStaff# coding: utf-8
+# 📂 apps/models/__init__.py
+
+from .admin_db import AdminUser
+from .admin_staff_db import AdminStaff
+from .financials_db import OrderFinancial
+from .marketers_db import Marketer
+# تم حذف OTPVerification
+from .orders_db import Order
+from .supplier_db import Supplier
+from .supplier_profile_db import SupplierProfile
 from .supplier_staff_db import SupplierStaff
+from .sync_log import SyncLog
+from .wallet_db import VendorWallet
+
+__all__ = [
+    'Supplier', 'AdminUser', 'AdminStaff', 'Marketer', 
+    'SupplierProfile', 'SupplierStaff', 
+    'SyncLog', 'VendorWallet', 'Order', 'OrderFinancial'
+]
 from .sync_log import SyncLog
 from .wallet_db import VendorWallet
 
