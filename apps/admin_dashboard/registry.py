cb25c473539f581e3e6f4e4ccc3bca429f07918e
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-06-26 19:35:17 +0300

    Update registry.py

--- apps/admin_dashboard/registry.py
+++ apps/admin_dashboard/registry.py
@@ -1,1 +1,8 @@
+# coding: utf-8
+from .routes import admin_dashboard_bp
 
+def register_module(app):
+    """
+    تسجيل موديول لوحة تحكم المسؤول في التطبيق.
+    """
+    app.register_blueprint(admin_dashboard_bp, url_prefix='/admin')
