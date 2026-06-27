60e3c7e437207354f51ccd26f8e69ebcf9a6a329
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-06-28 03:09:42 +0900

    Update registry.py

--- apps/suppliers_add/registry.py
+++ apps/suppliers_add/registry.py
@@ -1,1 +1,7 @@
+# 📂 apps/suppliers_add/registry.py
+from apps.suppliers_add.routes import suppliers_add_bp
 
+def register_module(app):
+    """تسجيل بلوبرينت إضافة الموردين"""
+    app.register_blueprint(suppliers_add_bp, url_prefix='/admin/suppliers')
+    print("✅ [Registry]: تم تسجيل موديول 'suppliers_add' بنجاح.")
