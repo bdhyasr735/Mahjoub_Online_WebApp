3cef61d951de2d110d4ce6d0162fea7062865d56
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-06-28 15:29:03 +0300

    Update __init__.py

--- apps/supplier_wallet/__init__.py
+++ apps/supplier_wallet/__init__.py
@@ -1,1 +1,7 @@
+# 📂 apps/supplier_wallet/__init__.py
 
+# استيراد الـ Blueprint لتسهيل الوصول إليه عند التسجيل
+from .routes import supplier_wallet_bp
+
+# استيراد الخدمات لتسهيل الوصول إليها عند تنفيذ المنطق المالي
+from .services import WalletService
