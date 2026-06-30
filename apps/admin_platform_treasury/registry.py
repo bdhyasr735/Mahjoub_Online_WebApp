c71f3edc4ab48b0175d5db2e6ac09505c9821f53
Jui 88 <jui586053@gmail.com>
2026-06-30 19:26:10 +0300

    Update registry.py

--- apps/admin_platform_treasury/registry.py
+++ apps/admin_platform_treasury/registry.py
@@ -1,7 +1,3 @@
-# 📂 apps/admin_platform_treasury/registry.py
 from .routes import treasury_bp
-
 def register_module(app):
-    # تسجيل موديول الخزينة
     app.register_blueprint(treasury_bp, url_prefix='/admin/treasury')
-    print("✅ [Registry]: تم تسجيل موديول 'treasury' بنجاح.")
