12cc971597d90ef3eb10e4e98b7498581ef04109
Jui 88 <jui586053@gmail.com>
2026-07-01 21:44:42 +0300

    Update registry.py

--- apps/admin_financial_management/registry.py
+++ apps/admin_financial_management/registry.py
@@ -1,12 +1,11 @@
 # coding: utf-8
 # 📂 apps/admin_financial_management/registry.py
 
-from apps.admin_financial_management.routes import admin_financial_bp
+from apps.admin_financial_management.routes import financial_mgmt_bp
 
 def register_module(app):
     """
     تسجيل موديول الإدارة المالية للطلبات.
-    يتم تسجيله على المسار /admin/financial
     """
-    app.register_blueprint(admin_financial_bp, url_prefix='/admin/financial')
-    print("✅ [Registry]: تم تسجيل موديول 'Admin Financial Management' بنجاح على المسار /admin/financial")
+    app.register_blueprint(financial_mgmt_bp, url_prefix='/admin/financial-management')
+    print("✅ [Registry]: تم تسجيل موديول 'Admin Financial Management' بنجاح.")
