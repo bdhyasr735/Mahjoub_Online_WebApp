ca4072390544eeb6de3a6506d108a8d9f0b0992b
Jui 88 <jui586053@gmail.com>
2026-07-04 20:22:11 +0300

    Update routes.py

--- apps/admin_suppliers_list/routes.py
+++ apps/admin_suppliers_list/routes.py
@@ -1,10 +1,9 @@
 # coding: utf-8
 # 📂 apps/admin_suppliers_list/routes.py
 
-from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask import Blueprint, render_template
 from flask_login import login_required
 from apps.models.supplier_db import Supplier
-from apps.extensions import db
 
 # تعريف الـ Blueprint
 suppliers_bp = Blueprint(
@@ -13,16 +12,9 @@ suppliers_bp = Blueprint(
     template_folder='templates'
 )
 
-# تأكد أن أسماء الدوال تطابق ما كتبته في HTML
 @suppliers_bp.route('/list')
 @login_required
 def list_suppliers():
-    suppliers = Supplier.query.all()
+    # جلب الموردين وعرضهم في الصفحة
+    suppliers = Supplier.query.order_by(Supplier.id.desc()).all()
     return render_template('admin_suppliers_list/admin_suppliers_list.html', suppliers=suppliers)
-
-# مثال لدالة الإضافة (أضف المسارات الأخرى التي تحتاجها)
-@suppliers_bp.route('/add')
-@login_required
-def add_supplier():
-    # منطق إضافة المورد
-    return "صفحة إضافة المورد"
