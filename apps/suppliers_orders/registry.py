0976682808d67b8a4d709c6afaab5b50892533bb
Jui 88 <jui586053@gmail.com>
2026-07-06 14:56:25 +0300

    Update registry.py

--- apps/suppliers_orders/registry.py
+++ apps/suppliers_orders/registry.py
@@ -1,7 +1,7 @@
-# coding: utf-8
+# 📂 apps/suppliers_orders/registry.py
+
 from apps.suppliers_orders.routes import suppliers_orders_bp
 
-# تأكد من أن اسم الموديول (suppliers_orders) لا يتكرر
 def register_module(app):
-    # نستخدم url_prefix خاص لتجنب أي تداخل
-    app.register_blueprint(suppliers_orders_bp, url_prefix='/suppliers_orders')
+    # التسجيل مع url_prefix فريد لضمان عدم التعارض
+    app.register_blueprint(suppliers_orders_bp, url_prefix='/suppliers/orders')
