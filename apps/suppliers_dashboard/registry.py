b7e622103403023d2f8b6efe3e3347bb11922003
Jui 88 <jui586053@gmail.com>
2026-07-06 22:46:53 +0300

    Update registry.py

--- apps/suppliers_dashboard/registry.py
+++ apps/suppliers_dashboard/registry.py
@@ -1,5 +1,22 @@
+# coding: utf-8
 # 📂 apps/suppliers_dashboard/registry.py
+
+# هذه البيانات ستسمح للنظام بمعرفة أيقونة واسم الموديول
+MODULE_NAME = "الرئيسية"
+MODULE_ICON = "fas fa-home"
+SHOW_IN_SUPPLIER = True
+
+# تعريف الروابط التي سيستخدمها النظام
+LINKS = {
+    'لوحة التحكم': 'suppliers_dashboard.dashboard'
+}
+
 def register_module(app):
-    from apps.suppliers_dashboard.routes import suppliers_dashboard_bp # استيراد محلي
+    """
+    تسجيل موديول لوحة التحكم (Dashboard)
+    """
+    from apps.suppliers_dashboard.routes import suppliers_dashboard_bp
+    
     if 'suppliers_dashboard' not in app.blueprints:
         app.register_blueprint(suppliers_dashboard_bp, url_prefix='/supplier')
+        print("✅ [Registry]: تم تسجيل 'suppliers_dashboard' بنجاح.")
