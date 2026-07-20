f6b60572c12290fa2a4bfaad38a70550bea4687e
mahjoubonline <mahjoubonline@outlook.sa>
2026-07-20 14:10:45 +0300

    Update product_media_extras.graphql.py

--- apps/services/product_media_extras.graphql.py
+++ apps/services/product_media_extras.graphql.py
@@ -5,6 +5,7 @@ PRODUCT_MEDIA_EXTRAS_QUERY = """
 query GetProductMediaAndExtras($qid: String!) {
   findProductByQid(qid: $qid) {
     data {
+      slug        # حقل الرابط (Slug)
       images {
         _id
         fileUrl
@@ -22,18 +23,27 @@ query GetProductMediaAndExtras($qid: String!) {
 MANAGE_MEDIA_AND_EXTRAS_MUTATION = """
 mutation ManageMediaAndExtras(
   $productId: ID!,
+  $slug: String!,       # تم إضافة حقل الرابط هنا
   $imageIds: [ID!]!,
   $tags: [String!]!,
   $collectionIds: [String!]!
 ) {
+  # تحديث الرابط (Slug) ضمن معلومات المنتج
+  updateProductInfo(id: $productId, updateProductInfoInput: { slug: $slug }) {
+    qid
+  }
+
+  # تحديث قائمة صور المنتج
   updateProductImages(id: $productId, data: $imageIds) {
     qid
   }
 
+  # تحديث وسوم المنتج
   updateProductInfo(id: $productId, updateProductInfoInput: { tags: $tags }) {
     qid
   }
 
+  # تحديث تصنيفات المنتج
   updateProductCollection(collectionIds: $collectionIds) {
     qid
   }
