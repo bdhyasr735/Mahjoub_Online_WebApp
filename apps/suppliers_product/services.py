f5843c4d11700042fb1d5385e0ef802a45c7c32e
mahjoubonline <mahjoubonline@outlook.sa>
2026-07-24 22:38:02 +0300

    Update services.py

--- apps/suppliers_product/services.py
+++ apps/suppliers_product/services.py
@@ -5,7 +5,7 @@ from apps.services.product_sync_service import ProductSyncService as QumraSyncSe
 from apps.services.product_mapping_service import product_mapping
 from apps.services.product_ident_mutation import product_ident
 from apps.models.product_supplier_map import ProductSupplierMapping
-from apps.models import Supplier
+from apps.models import Supplier  # ✅ استيراد صحيح
 from apps.suppliers_product.helpers import compress_image
 import logging
 import time
