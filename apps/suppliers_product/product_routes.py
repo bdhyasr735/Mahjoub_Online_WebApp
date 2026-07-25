a313e8ec3f0902d4a757a9a0d9a658e9026870fb
mahjoubonline <mahjoubonline@outlook.sa>
2026-07-25 18:48:10 +0300

    Rename suppliers_product_routes.py to product_routes.py

--- apps/suppliers_product/product_routes.py
+++ apps/suppliers_product/product_routes.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-# 📂 apps/suppliers_product/routes.py
+# 📂 product_routes.py
 
 from flask import Blueprint, render_template, request, session, abort, jsonify, flash, redirect, url_for
 from flask_login import login_required, current_user
