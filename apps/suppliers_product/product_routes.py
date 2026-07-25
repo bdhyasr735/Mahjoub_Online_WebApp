f394829eadee3e63b1bc4dc87193ee3dd579e603
mahjoubonline <mahjoubonline@outlook.sa>
2026-07-25 20:27:56 +0300

    Update product_routes.py

--- apps/suppliers_product/product_routes.py
+++ apps/suppliers_product/product_routes.py
@@ -35,7 +35,7 @@ def index():
     """
     try:
         page = request.args.get('page', 1, type=int)
-        per_page = request.args.get('per_page', 20, type=int)  # تصحيح الخطأ الإملائي per_per إلى per_page
+        per_page = request.args.get('per_page', 20, type=int)
         search = request.args.get('search', '', type=str)
         status = request.args.get('status', 'all', type=str)
 
