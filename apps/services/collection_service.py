d1c8fa337cb4c8b05d8274a693c0e553eee47861
mahjoubonline <mahjoubonline@outlook.sa>
2026-07-26 13:30:25 +0300

    Update collection_service.py

--- apps/services/collection_service.py
+++ apps/services/collection_service.py
@@ -1,19 +1,25 @@
+# coding: utf-8
+# 📂 apps/services/collection_service.py
+
 """
 خدمة المجموعات - Collection Service
 """
 
 from typing import List, Optional, Dict
-from graphql_client import GraphQLClient
+from .graphql_client import GraphQLClient
 
 
 class CollectionService:
     """خدمة لإدارة المجموعات"""
     
-    def __init__(self, client: GraphQLClient):
+    def __init__(self, client: GraphQLClient, queries_path: str = 'apps/services/collection_queries.graphql'):
         self.client = client
         
-        with open('collection_queries.graphql', 'r') as f:
-            self.queries = f.read()
+        try:
+            with open(queries_path, 'r', encoding='utf-8') as f:
+                self.queries = f.read()
+        except FileNotFoundError:
+            self.queries = ""
     
     def get_all_collections(self) -> List[Dict]:
         """
@@ -57,12 +63,12 @@ class CollectionService:
         }
         """
         
-        result = self.client.execute(query, {})
+        result = self.client.execute(query, {}) or {}
         return result.get('findAllCollections', [])
     
     def get_collection_by_qid(self, qid: str) -> Optional[Dict]:
         """
-        جلب مجموعة بواسطة QID
+        جلب مجموعة بواسطة QID أو المعرف
         
         Args:
             qid: المعرف الفريد للمجموعة
@@ -114,5 +120,5 @@ class CollectionService:
         """
         
         variables = {"id": qid}
-        result = self.client.execute(query, variables)
-        return result.get
+        result = self.client.execute(query, variables) or {}
+        return result.get('findCollectionByQid')
