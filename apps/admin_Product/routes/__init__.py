52c5202c24988d5099e544316dbe90651d5c9d80
mahjoubonline <mahjoubonline@outlook.sa>
2026-07-26 19:58:24 +0300

    Update __init__.py

--- apps/admin_Product/routes/__init__.py
+++ apps/admin_Product/routes/__init__.py
@@ -1,10 +1,18 @@
 # coding: utf-8
 # 📂 apps/admin_Product/routes/__init__.py
 
+import os
 from flask import Blueprint
 
-# إنشاء Blueprint الرئيسي
-admin_product_bp = Blueprint('admin_product_bp', __name__, template_folder='templates')
+# ✅ المسار المطلق لمجلد القوالب
+template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'templates')
+
+# إنشاء Blueprint الرئيسي مع المسار المطلق للقوالب
+admin_product_bp = Blueprint(
+    'admin_product_bp', 
+    __name__, 
+    template_folder=template_dir
+)
 
 # ✅ استيراد جميع الـ Routes
 from . import sync
