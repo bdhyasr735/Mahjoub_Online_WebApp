2c8f30bb072c815f02aeeff8f2d71821f66023b4
mahjoubonline <mahjoubonline@outlook.sa>
2026-07-26 16:58:23 +0300

    Update __init__.py

--- apps/admin_Product/__init__.py
+++ apps/admin_Product/__init__.py
@@ -1,1 +1,17 @@
+# coding: utf-8
+# 📂 apps/admin_Product/routes/__init__.py
 
+from flask import Blueprint
+
+# إنشاء Blueprint الرئيسي
+admin_product_bp = Blueprint('admin_product_bp', __name__, template_folder='templates')
+
+# استيراد جميع الـ Routes
+from . import products
+from . import sync
+from . import reviews
+from . import crud
+from . import stats
+
+# ✅ تصدير الـ Blueprint فقط
+__all__ = ['admin_product_bp']
