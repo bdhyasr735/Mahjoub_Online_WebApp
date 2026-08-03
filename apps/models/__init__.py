71e26af76305ac45f384b6c12f1cad3e71ca3b84
mahjoubonline <mahjoubonline@outlook.sa>
2026-08-03 23:52:22 +0300

    Update __init__.py

--- apps/models/__init__.py
+++ apps/models/__init__.py
@@ -7,7 +7,7 @@
 
 from apps.extensions import db
 
-# استخدام الاستيراد المطلق (Absolute Imports) حصرياً لمنع ازدواجية تسجيل الموديلات في ذاكرة SQLAlchemy
+# استخدام الاستيراد المطلق (Absolute Imports) مع فصل الموديلات المستقلة
 from apps.models.supplier_db import Supplier
 from apps.models.admin_db import AdminUser
 from apps.models.marketer_db import Marketer
@@ -16,7 +16,8 @@ from apps.models.supplier_profile_db import SupplierProfile
 from apps.models.supplier_staff_db import SupplierStaff
 from apps.models.wallet_db import SupplierWallet, WalletTransaction
 from apps.models.financials_db import OrderFinancial
-from apps.models.orders_db import Order, OrderItem
+from apps.models.orders_db import Order
+from apps.models.order_items_db import OrderItem
 from apps.models.product_db import Product
 from apps.models.product_supplier_map import ProductSupplierMapping
 from apps.models.sync_log import SyncLog
