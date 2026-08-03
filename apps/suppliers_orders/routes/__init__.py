93208cd18cc0f0c34eb7d71d60f3941f7de7b95f
mahjoubonline <mahjoubonline@outlook.sa>
2026-08-03 11:02:17 +0300

    Update __init__.py

--- apps/suppliers_orders/routes/__init__.py
+++ apps/suppliers_orders/routes/__init__.py
@@ -1,1 +1,5 @@
-
+suppliers_product_bp = Blueprint(...)
+from . import sync, reviews, stats, products
+from .products import register_products_route
+register_products_route(suppliers_product_bp)
+__all__ = ['suppliers_product_bp']
