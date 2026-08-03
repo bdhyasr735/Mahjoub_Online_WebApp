554fab195ecdc5eac8a55f6f7ae4176d62d3d3ca
mahjoubonline <mahjoubonline@outlook.sa>
2026-08-03 11:06:44 +0300

    Update __init__.py

--- apps/suppliers_orders/__init__.py
+++ apps/suppliers_orders/__init__.py
@@ -1,1 +1,11 @@
+# coding: utf-8
+# 📂 apps/suppliers_orders/__init__.py
 
+"""
+موديول طلبات الزبائن للموردين.
+"""
+
+# لا نقوم باستيراد أي شيء من routes هنا لتجنب Circular Import.
+# التسجيل سيتم عبر registry.py عند بدء التطبيق.
+
+__all__ = []
