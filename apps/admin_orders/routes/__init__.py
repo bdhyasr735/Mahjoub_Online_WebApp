67238f64af49e635ad89ff999cc7258eea8fe95f
mahjoubonline <mahjoubonline@outlook.sa>
2026-08-04 20:02:21 +0300

    Update __init__.py

--- apps/admin_orders/routes/__init__.py
+++ apps/admin_orders/routes/__init__.py
@@ -1,15 +1,4 @@
 # coding: utf-8
-# 📂 apps/admin_orders/routes/__init__.py
+# 📂 apps/admin_orders/__init__.py
 
-from flask import Blueprint
-
-# تعريف الـ Blueprint الأساسي أولاً
-admin_orders_bp = Blueprint(
-    'admin_orders', 
-    __name__, 
-    template_folder='../templates', 
-    url_prefix='/admin/orders'
-)
-
-# استيراد ملفات المسارات الفرعية لتسجيلها
-from apps.admin_orders.routes import orders, actions
+from apps.admin_orders.routes import admin_orders_bp
