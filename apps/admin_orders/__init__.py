d9734c6efa6747a539d21ba473035b86e38dff75
mahjoubonline <mahjoubonline@outlook.sa>
2026-08-04 22:54:14 +0300

    Update __init__.py

--- apps/admin_orders/__init__.py
+++ apps/admin_orders/__init__.py
@@ -1,11 +1,1 @@
-# coding: utf-8
-# 📂 apps/admin_orders/__init__.py
-
-"""
-موديول إدارة الطلبات - يجمع الـ Blueprints ويصدرها للتطبيق.
-"""
-
 from .routes import admin_orders_bp, actions_bp
-
-# تصدير الـ Blueprints ليتمكن ملف registry.py من استيرادها
-__all__ = ['admin_orders_bp', 'actions_bp']
