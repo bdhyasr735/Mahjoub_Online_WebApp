6ffa7dd1b8509c92eb0f3e32fed0af5e0c6ee79a
mahjoubonline <mahjoubonline@outlook.sa>
2026-08-04 19:24:47 +0300

    Remove admin_orders Blueprint initialization

--- apps/admin_orders/routes/__init__.py
+++ apps/admin_orders/routes/__init__.py
@@ -1,19 +1,1 @@
-# coding: utf-8
-# 📂 apps/admin_orders/__init__.py
 
-import os
-from flask import Blueprint
-
-template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
-static_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')
-
-# تعريف الـ Blueprint هنا
-admin_orders_bp = Blueprint(
-    'admin_orders_bp',
-    __name__,
-    template_folder=template_dir,
-    static_folder=static_dir
-)
-
-# استيراد الملفات الفرعية في النهاية
-from apps.admin_orders.routes import orders, actions
