71f0433df73758ed227de1bbfdd32ec9ec0020c0
mahjoubonline <mahjoubonline@outlook.sa>
2026-08-04 19:36:42 +0300

    Update __init__.py

--- apps/admin_orders/routes/__init__.py
+++ apps/admin_orders/routes/__init__.py
@@ -1,4 +1,14 @@
 # coding: utf-8
-# 📂 apps/admin_orders/routes/__init__.py
+# 📂 apps/admin_orders/__init__.py
 
+from flask import Blueprint
+
+admin_orders_bp = Blueprint(
+    'admin_orders_bp',
+    __name__,
+    template_folder='templates',
+    static_folder='static'
+)
+
+# استيراد المسارات لتسجيلها تلقائياً مع الـ Blueprint
 from apps.admin_orders.routes import orders, actions
