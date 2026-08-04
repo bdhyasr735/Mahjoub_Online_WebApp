a316741c4ad5f6637a743d90d159f4496bb52383
mahjoubonline <mahjoubonline@outlook.sa>
2026-08-04 23:00:10 +0300

    Update __init__.py

--- apps/admin_orders/routes/__init__.py
+++ apps/admin_orders/routes/__init__.py
@@ -1,10 +1,7 @@
 # coding: utf-8
-# 📂 apps/admin_orders/__init__.py
+# 📂 apps/admin_orders/routes/__init__.py
 
-"""
-موديول إدارة الطلبات - يجمع الـ Blueprints ويصدرها للتطبيق.
-"""
-
-from .routes import admin_orders_bp, actions_bp
+from .orders import admin_orders_bp
+from .actions import actions_bp
 
 __all__ = ['admin_orders_bp', 'actions_bp']
