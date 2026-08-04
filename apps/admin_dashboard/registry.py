3c17a858e6cab3d2b611957644d348979e99a246
mahjoubonline <mahjoubonline@outlook.sa>
2026-08-04 21:17:21 +0300

    Update registry.py

--- apps/admin_dashboard/registry.py
+++ apps/admin_dashboard/registry.py
@@ -8,7 +8,7 @@ MODULE_ICON = "fa-tachometer-alt"
 SHOW_IN_SUPPLIER = False
 
 LINKS = {
-    "📊 الرئيسية": "admin_dashboard_bp.dashboard"
+    "admin_dashboard_bp.dashboard": "📊 الرئيسية"   # ✅ المفتاح = endpoint، القيمة = النص
 }
 
 def register_module(app):
