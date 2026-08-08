ef8362571b6961fe277bfbe01b9f9ad4833d5317
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-08-08 22:43:01 +0300

    Update __init__.py

--- apps/admin_permissions/__init__.py
+++ apps/admin_permissions/__init__.py
@@ -1,1 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+حزمة إدارة الصلاحيات - منصة محجوب أونلاين
+"""
+from apps.admin_permissions.registry import MODULE_NAME, MODULE_ICON, register_module
 
+__all__ = ["MODULE_NAME", "MODULE_ICON", "register_module"]
