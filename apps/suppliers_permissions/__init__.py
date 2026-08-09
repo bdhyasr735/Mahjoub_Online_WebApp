fa64f8f19c5f112cccac7247c4056d9ce8439186
mahjoubonline <mahjoubonline@outlook.sa>
2026-08-09 12:35:11 +0300

    Update __init__.py

--- apps/suppliers_permissions/__init__.py
+++ apps/suppliers_permissions/__init__.py
@@ -1,1 +1,13 @@
+# -*- coding: utf-8 -*-
+# 📂 apps/supplier_permissions/__init__.py
 
+from flask import Blueprint
+
+supplier_perms_bp = Blueprint(
+    'supplier_perms',
+    __name__,
+    template_folder='templates',
+    url_prefix='/supplier/permissions'
+)
+
+from . import routes
