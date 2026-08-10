101c654047fec6ee24acd95954826263c48e324d
mahjoubonline <mahjoubonline@outlook.sa>
2026-08-10 23:04:17 +0300

    Update __init__.py

--- apps/admin_Product/__init__.py
+++ apps/admin_Product/__init__.py
@@ -1,1 +1,16 @@
+"""
+admin_Product Blueprint Package
+إدارة المنتجات والمتغيرات لمتجر محجوب أونلاين (www.mahjoub.online)
+"""
 
+from flask import Blueprint
+
+admin_product_bp = Blueprint(
+    'admin_Product',
+    __name__,
+    template_folder='templates',
+    static_folder='static',
+    url_prefix='/admin/products'
+)
+
+from . import routes
