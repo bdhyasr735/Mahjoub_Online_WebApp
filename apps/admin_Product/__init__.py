63cc8f7c91e7e2596ee6d2c476e5c2e84bcee74d
mahjoubonline <mahjoubonline@outlook.sa>
2026-08-10 21:51:59 +0300

    Delete admin_Product Blueprint initialization
    Remove the admin_Product Blueprint package and its imports.

--- apps/admin_Product/__init__.py
+++ apps/admin_Product/__init__.py
@@ -1,16 +1,1 @@
-"""
-admin_Product Blueprint Package
-إدارة المنتجات والمتغيرات لمتجر محجوب أونلاين (www.mahjoub.online)
-"""
 
-from flask import Blueprint
-
-admin_product_bp = Blueprint(
-    'admin_Product',
-    __name__,
-    template_folder='templates',
-    static_folder='static',
-    url_prefix='/admin/products'
-)
-
-from . import routes
