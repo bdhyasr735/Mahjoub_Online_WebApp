e764ea37b72a5b3c40277dfdd3368acbf21acbd5
mahjoubonline <mahjoubonline@outlook.sa>
2026-08-10 21:05:43 +0300

    Update __init__.py

--- apps/admin_Product/__init__.py
+++ apps/admin_Product/__init__.py
@@ -1,4 +1,3 @@
-# apps/admin_Product/__init__.py
 from flask import Blueprint
 
 admin_product_bp = Blueprint(
@@ -6,7 +5,6 @@ admin_product_bp = Blueprint(
     __name__,
     template_folder='templates',
     static_folder='static'
-    # أزلنا url_prefix من هنا
 )
 
 from . import routes
