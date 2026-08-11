0ba734341b5ba990ae9c8bd85843fc648da093c0
mahjoubonline <mahjoubonline@outlook.sa>
2026-08-11 12:29:27 +0300

    Update __init__.py

--- apps/admin_product/__init__.py
+++ apps/admin_product/__init__.py
@@ -1,1 +1,15 @@
+# -*- coding: utf-8 -*-
+# 📂 apps/admin_product/__init__.py
 
+from flask import Blueprint
+
+# تعريف الـ Blueprint الخاص بإدارة المنتجات ومتجر محجوب أونلاين
+admin_product_bp = Blueprint(
+    'admin_product',
+    __name__,
+    template_folder='templates',
+    static_folder='static'
+)
+
+# استيراد المسارات لضمان تسجيلها داخل الـ Blueprint
+from . import routes
