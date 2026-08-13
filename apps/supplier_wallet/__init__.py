70e13d3a9761dac76b2763e4b308ed05e325e4c2
mahjoubonline <mahjoubonline@outlook.sa>
2026-08-13 19:01:46 +0300

    Add export_routes to supplier_wallet module imports

--- apps/supplier_wallet/__init__.py
+++ apps/supplier_wallet/__init__.py
@@ -7,6 +7,6 @@ Package initialization for apps/supplier_wallet
 from .registry import supplier_wallet_bp, register_module
 
 # استيراد ملفات المسارات لضمان تسجيل الـ Routes وربطها بالـ Blueprint
-from apps.supplier_wallet import wallet_routes, withdraw_routes
+from apps.supplier_wallet import wallet_routes, withdraw_routes, export_routes
 
 __all__ = ['supplier_wallet_bp', 'register_module']
