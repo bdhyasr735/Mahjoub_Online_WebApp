37e4af96e9f26c292257a5b0c18bfe60dab9566a
mahjoubonline <mahjoubonline@outlook.sa>
2026-08-14 20:43:44 +0300

    Create Admin Treasury Blueprint
    Added a blueprint for the Admin Treasury module in the application.

--- apps/admin_treasury/ __init__.py
+++ apps/admin_treasury/ __init__.py
@@ -1,1 +1,23 @@
+"""
+apps/admin_treasury/__init__.py
+حزمة موديول الخزينة المركزية وحسابات الضمان
+مشروع Mahjoub Online WebApp
+"""
 
+from flask import Blueprint
+
+def create_admin_treasury_blueprint():
+    """
+    إنشاء وتهيئة مخطط الخزينة المركزية (Admin Treasury Blueprint)
+    """
+    treasury_bp = Blueprint(
+        'admin_treasury',
+        __name__,
+        url_prefix='/admin/treasury',
+        template_folder='templates'
+    )
+
+    from .routes import treasury_controller
+    treasury_bp.register_blueprint(treasury_controller.bp)
+
+    return treasury_bp
