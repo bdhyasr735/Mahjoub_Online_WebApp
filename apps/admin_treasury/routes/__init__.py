8bbe5c478ff00de2b4fbe3fc16c6863a648dc5dc
mahjoubonline <mahjoubonline@outlook.sa>
2026-08-14 23:56:22 +0300

    Update __init__.py

--- apps/admin_treasury/routes/__init__.py
+++ apps/admin_treasury/routes/__init__.py
@@ -1,11 +1,9 @@
-from flask import Blueprint
+# -*- coding: utf-8 -*-
+# 📂 apps/admin_treasury/routes/__init__.py
 
-admin_treasury_bp = Blueprint(
-    'admin_treasury',
-    __name__,
-    template_folder='../templates',
-    url_prefix='/admin/treasury'
-)
+"""
+هذا الملف يضمن تحميل المتحكمات (Controllers) وتسجيل المسارات (Routes) 
+التابعة لموديول الخزينة داخل الـ Blueprint.
+"""
 
-# استيراد المتحكم هنا ليتم تسجيل المسارات
-from apps.admin_treasury.routes import treasury_controller
+from . import treasury_controller
