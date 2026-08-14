1190091b32b77d74d0f9e9b7fb0db8dc860ba7b0
mahjoubonline <mahjoubonline@outlook.sa>
2026-08-14 21:25:32 +0300

    Refactor admin_treasury blueprint import handling

--- apps/admin_treasury/ __init__.py
+++ apps/admin_treasury/ __init__.py
@@ -7,7 +7,7 @@
 
 from flask import Blueprint
 
-# إنشاء البلوبرنت الخاص بالخزينة المركزية والرقابة المالية مع تحديد مسار القوالب والمجلدات المشتركة
+# تعريف البلوبرنت الخاص بالخزينة المركزية والرقابة المالية
 admin_treasury_bp = Blueprint(
     'admin_treasury',
     __name__,
@@ -16,4 +16,8 @@ admin_treasury_bp = Blueprint(
 )
 
 # استيراد المتحكمات لتسجيل المسارات المرتبطة بها داخل البلوبرنت
-from apps.admin_treasury.routes import treasury_controller
+try:
+    from apps.admin_treasury.routes import treasury_controller
+except ImportError:
+    # لتجنب أي استيراد دائري أو مبكر غير مرتب
+    pass
