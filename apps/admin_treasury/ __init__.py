debb048089b0949f8278f72f5891fa74ce878331
mahjoubonline <mahjoubonline@outlook.sa>
2026-08-14 22:56:53 +0300

    Update  __init__.py

--- apps/admin_treasury/ __init__.py
+++ apps/admin_treasury/ __init__.py
@@ -1,12 +1,2 @@
-# -*- coding: utf-8 -*-
-# 📂 apps/admin_treasury/__init__.py
-"""
-ملف تهيئة وبناء البلوبرنت لموديول الرقابة المالية (الخزينة المركزية) وحسابات الضمان
-مشروع Mahjoub Online WebApp
-"""
-
-# استورد الـ Blueprint من مكانه الحقيقي (من داخل مجلد routes)
 from .routes.treasury_controller import admin_treasury_bp
-
-# أخبر بايثون أن هذا هو المتغير العمومي للمجلد
 __all__ = ["admin_treasury_bp"]
