95749c6cea4659047680e80ab7f19c44b6dcf8df
mahjoubonline <mahjoubonline@outlook.sa>
2026-08-14 22:39:44 +0300

    Update __init__.py

--- apps/admin_treasury/routes/__init__.py
+++ apps/admin_treasury/routes/__init__.py
@@ -1,1 +1,22 @@
+# -*- coding: utf-8 -*-
+# 📂 apps/admin_treasury/routes/__init__.py
+"""
+حزمة المتحكمات لموديول الرقابة المالية والخزينة المركزية
+مشروع Mahjoub Online WebApp
+"""
 
+from flask import render_template
+from flask_login import login_required
+from apps.admin_treasury import admin_treasury_bp
+
+@admin_treasury_bp.route('/', methods=['GET'])
+@login_required
+def treasury_index():
+    """
+    الصفحة الرئيسية للرقابة المالية والخزينة المركزية
+    """
+    kpi = {
+        "total_treasury_balance": 0.0,
+        "currency": "ريال يمني"
+    }
+    return render_template('admin/admin_treasury.html', kpi=kpi)
