36896bc48deaaa605333de691fddfcfb14cc527f
mahjoubonline <mahjoubonline@outlook.sa>
2026-08-15 21:28:12 +0300

    Update __init__.py

--- apps/admin_suppliers_wallets/__init__.py
+++ apps/admin_suppliers_wallets/__init__.py
@@ -1,1 +1,15 @@
+from flask import Blueprint
 
+def create_admin_suppliers_wallets_blueprint():
+    bp = Blueprint(
+        'admin_suppliers_wallets',
+        __name__,
+        template_folder='templates',
+        static_folder='static',
+        url_prefix='/admin/suppliers-wallets'
+    )
+    
+    from .routes import suppliers_wallets_controller
+    bp.register_blueprint(suppliers_wallets_controller.bp)
+    
+    return bp
