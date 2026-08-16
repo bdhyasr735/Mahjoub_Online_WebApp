7b10f0f963a1cb560d7cd55247b55fbb174e3d42
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-08-16 15:58:48 +0300

    Update __init__.py

--- apps/admin_suppliers_wallets/routes/__init__.py
+++ apps/admin_suppliers_wallets/routes/__init__.py
@@ -1,3 +1,15 @@
-from .suppliers_wallets_controller import bp as suppliers_wallets_bp
+from flask import Blueprint
 
-__all__ = ['suppliers_wallets_bp']
+def create_admin_suppliers_wallets_blueprint():
+    bp = Blueprint(
+        'admin_suppliers_wallets',
+        __name__,
+        template_folder='templates',
+        static_folder='static',
+        url_prefix='/admin/suppliers-wallets'
+    )
+    
+    from .routes import suppliers_wallets_controller
+    bp.register_blueprint(suppliers_wallets_controller.bp)
+    
+    return bp
