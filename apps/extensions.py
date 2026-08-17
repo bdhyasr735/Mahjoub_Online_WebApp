f0397f6e274f0e4cd74ac0b85f6b2fa92332643a
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-08-17 09:50:38 +0300

    Update extensions.py

--- apps/extensions.py
+++ apps/extensions.py
@@ -4,10 +4,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from flask_wtf.csrf import CSRFProtect # تم إضافة هذا الاستيراد
+from flask_wtf.csrf import CSRFProtect
 from sqlalchemy import MetaData
 from sqlalchemy.orm import joinedload
 from flask import session
+from flask_limiter import Limiter              # 1. إضافة الاستيراد
+from flask_limiter.util import get_remote_address # 2. إضافة دالة تحديد عنوان IP
 
 # تعريف الـ Naming Convention لمنع تعارض الأسماء في قاعدة البيانات
 metadata = MetaData(
@@ -23,7 +25,14 @@ metadata = MetaData(
 db = SQLAlchemy(metadata=metadata)
 migrate = Migrate()
 login_manager = LoginManager()
-csrf = CSRFProtect() # تعريف الـ csrf هنا ليتم استخدامه في __init__.py
+csrf = CSRFProtect()
+
+# 3. تعريف الـ limiter مركزياً
+limiter = Limiter(
+    key_func=get_remote_address, 
+    default_limits=["5000 per day", "1000 per hour"],
+    storage_uri="memory://"
+)
 
 @login_manager.user_loader
 def load_user(user_id):
