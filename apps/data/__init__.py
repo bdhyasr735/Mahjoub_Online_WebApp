e8d6c3b5d1e33732c1726b2daa987958b0356f84
mahjoubonline <mahjoubonline@outlook.sa>
2026-08-18 13:47:02 +0300

    Update __init__.py

--- apps/data/__init__.py
+++ apps/data/__init__.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 # 📂 apps/data/__init__.py
 
-"""
-بيانات ثابتة للتطبيق
-"""
+from apps.data.yemen_banks import YEMEN_BANKS as yemen_banks
+from apps.data.financial_companies import FINANCIAL_COMPANIES as financial_companies
+
+__all__ = ['yemen_banks', 'financial_companies']
