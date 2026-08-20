b59a99c933890edadab399a9aa138310b386e2e4
mahjoubonline <mahjoubonline@outlook.sa>
2026-08-20 11:56:56 +0300

    Update __init__.py

--- apps/supplier_wallet/routes.py/__init__.py
+++ apps/supplier_wallet/routes.py/__init__.py
@@ -3,7 +3,7 @@
 مسارات ومتحكمات محفظة الموردين (Flask Route Blueprints)
 """
 
-from apps.suppliers_wallet.routes.wallet_routes import wallet_bp
-from apps.suppliers_wallet.routes.admin_routes import admin_wallet_bp
+from apps.supplier_wallet.routes.wallet_routes import wallet_bp
+from apps.supplier_wallet.routes.admin_routes import admin_wallet_bp
 
 __all__ = ['wallet_bp', 'admin_wallet_bp']
