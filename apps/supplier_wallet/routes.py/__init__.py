31f4603958880358cd7823203474878524c99c24
mahjoubonline <mahjoubonline@outlook.sa>
2026-08-20 23:13:54 +0300

    Update __init__.py

--- apps/supplier_wallet/routes.py/__init__.py
+++ apps/supplier_wallet/routes.py/__init__.py
@@ -1,8 +1,12 @@
 # coding: utf-8
+# 📂 apps/supplier_wallet/routes/__init__.py
+
 """
-📂 apps/supplier_wallet/routes/__init__.py
-حزمة مسارات محفظة المورد (Supplier Wallet Routes Package)
-- ربط واستيراد كافة ملفات التحكم والمسارات التابعة لمحفظة المورد
+حزمة مسارات موديول المحفظة (Supplier Wallet Routes Package)
+- تقوم بتجميع مسارات الموردين ومسارات الإدارة وتصديرها تلقائياً.
 """
 
-from .wallet_routes import *
+from .wallet_routes import supplier_wallet_bp
+from .admin_routes import admin_wallet_bp
+
+__all__ = ['supplier_wallet_bp', 'admin_wallet_bp']
