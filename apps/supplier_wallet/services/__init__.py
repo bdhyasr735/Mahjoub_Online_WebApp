104fcff88e6ef1fd7bccd250127d428cef074a2b
mahjoubonline <mahjoubonline@outlook.sa>
2026-08-20 11:56:28 +0300

    Update __init__.py

--- apps/supplier_wallet/services/__init__.py
+++ apps/supplier_wallet/services/__init__.py
@@ -3,7 +3,7 @@
 خدمات المحفظة والعمليات المالية المعتمدة (Business Logic Services)
 """
 
-from apps.suppliers_wallet.services.wallet_service import WalletService
-from apps.suppliers_wallet.services.notification_service import NotificationService
+from apps.supplier_wallet.services.wallet_service import WalletService
+from apps.supplier_wallet.services.notification_service import NotificationService
 
 __all__ = ['WalletService', 'NotificationService']
