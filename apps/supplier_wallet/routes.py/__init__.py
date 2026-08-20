13be1c8bbfc4dddddf103b9bd7da95fbed7cd765
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-08-21 01:10:03 +0300

    Update __init__.py

--- apps/supplier_wallet/routes.py/__init__.py
+++ apps/supplier_wallet/routes.py/__init__.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint
 
-# تعريف الـ Blueprint الخاص بالمورد هنا أو استيراده من wallet_routes
-from .wallet_routes import supplier_wallet_bp
+supplier_wallet_bp = Blueprint(
+    'supplier_wallet',
+    __name__,
+    template_folder='templates',
+    static_folder='static'
+)
+
+# استيراد مجلد المسارات في نهاية الملف لتسجيلها بعد إنشاء الـ Blueprint
+from .routes import wallet_routes
