6d478986f3cb601ad08eeafe21540c2bd932d097
mahjoubonline <mahjoubonline@outlook.sa>
2026-08-20 13:42:03 +0300

    Delete apps/admin_suppliers_wallets directory

--- apps/admin_suppliers_wallets/routes/__init__.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-"""
-مسارات ومتحكمات محفظة الموردين (Flask Route Blueprints)
-"""
-
-from apps.suppliers_wallet.routes.wallet_routes import wallet_bp
-from apps.suppliers_wallet.routes.admin_routes import admin_wallet_bp
-
-__all__ = ['wallet_bp', 'admin_wallet_bp']
--- apps/admin_suppliers_wallets/services/__init__.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-"""
-خدمات المحفظة والعمليات المالية المعتمدة (Business Logic Services)
-"""
-
-from apps.suppliers_wallet.services.wallet_service import WalletService
-from apps.suppliers_wallet.services.notification_service import NotificationService
-
-__all__ = ['WalletService', 'NotificationService']
