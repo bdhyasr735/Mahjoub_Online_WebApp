cffd3f968ade17dd011f8950c07e95ef46ce26b9
mahjoubonline <mahjoubonline@outlook.sa>
2026-08-20 14:59:03 +0300

    Update __init__.py

--- apps/supplier_wallet/routes.py/__init__.py
+++ apps/supplier_wallet/routes.py/__init__.py
@@ -1,7 +1,1 @@
-# coding: utf-8
-# 📂 apps/supplier_wallet/__init__.py
 
-from .registry import supplier_wallet_bp, register_module
-from .routes.admin_routes import admin_wallet_bp
-
-__all__ = ['supplier_wallet_bp', 'admin_wallet_bp', 'register_module']
