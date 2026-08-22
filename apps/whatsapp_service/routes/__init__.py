816cc41286f6dc66abcfe6af9b4ce97db9a4d68a
mahjoubonline <mahjoubonline@outlook.sa>
2026-08-22 22:43:24 +0300

    Update __init__.py

--- apps/whatsapp_service/routes/__init__.py
+++ apps/whatsapp_service/routes/__init__.py
@@ -5,11 +5,10 @@ import os
 from flask import Blueprint
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-# تعديل المسار ليشير مباشرة إلى مجلد الـ templates داخل موديول whatsapp_service
 template_dir = os.path.abspath(os.path.join(basedir, '../templates'))
 
 # تعريف الـ Blueprint الرئيسي مع تحديد مسار القوالب الصحيح
 whatsapp_bp = Blueprint('whatsapp_service', __name__, template_folder=template_dir)
 
-# استيراد الملفات الفرعية لربط المسارات (Routes) بالـ Blueprint
+# استيراد الملفات الفرعية لربط المسارات (Routes) بالـ Blueprint مرة واحدة فقط
 from . import webhook, dashboard, actions, api
