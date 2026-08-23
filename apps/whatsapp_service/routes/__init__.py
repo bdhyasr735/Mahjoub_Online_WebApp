e2540af3fb259a9658a10ffbe1e6396d6481dd3f
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-08-24 02:42:47 +0300

    Update __init__.py

--- apps/whatsapp_service/routes/__init__.py
+++ apps/whatsapp_service/routes/__init__.py
@@ -1,15 +1,14 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
 # 📂 apps/whatsapp_service/routes/__init__.py
 
 from flask import Blueprint
 
-# تعريف الـ Blueprint الخاص بخدمة الواتساب
 whatsapp_bp = Blueprint(
     'whatsapp_service',
     __name__,
-    template_folder='templates',
-    static_folder='static'
+    template_folder='../templates',
+    static_folder='../static'
 )
 
-# استيراد ملفات المسارات لكي يتم تسجيلها وتفعيلها مع الـ Blueprint
-from . import dashboard, api, actions, webhook
+# استيراد ملفات المسارات الداخلية لتفعيل الـ Routes
+from apps.whatsapp_service.routes import views, api
