db9f9e8894397d268ed3700d5f558e2dd2b9028e
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-08-24 02:29:26 +0300

    Update __init__.py

--- apps/whatsapp_service/__init__.py
+++ apps/whatsapp_service/__init__.py
@@ -1,1 +1,16 @@
 
+# coding: utf-8
+# 📂 apps/whatsapp_service/__init__.py
+
+from flask import Blueprint
+
+# تعريف البلوبرنت الأساسي للخدمة (يتم استخدامه وتسجيله ديناميكياً عبر registry.py)
+whatsapp_bp = Blueprint(
+    'whatsapp_service',
+    __name__,
+    template_folder='templates',
+    static_folder='static'
+)
+
+# استيراد المسارات والملفات التابعة للموديول لضمان تسجيل الروابط عند تحميله
+from apps.whatsapp_service import routes
