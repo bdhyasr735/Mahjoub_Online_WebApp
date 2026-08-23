ac5816f9470b10df4efdcb37701c4381e3b64320
mahjoubonline <mahjoubonline@outlook.sa>
2026-08-23 14:26:06 +0300

    Update __init__.py

--- apps/whatsapp_service/__init__.py
+++ apps/whatsapp_service/__init__.py
@@ -6,6 +6,5 @@ WhatsApp Service Package Initialization
 """
 
 from .registry import register_module, SERVICE_METADATA
-from .routes.whatsapp_controller import whatsapp_bp
 
-__all__ = ['register_module', 'SERVICE_METADATA', 'whatsapp_bp']
+__all__ = ['register_module', 'SERVICE_METADATA']
