e7128ca3d2574de6ca932b13390ab43942dcec2b
mahjoubonline <mahjoubonline@outlook.sa>
2026-08-23 23:57:56 +0300

    Update __init__.py

--- apps/whatsapp_service/__init__.py
+++ apps/whatsapp_service/__init__.py
@@ -1,12 +1,1 @@
-# coding: utf-8
-# 📂 apps/whatsapp_service/__init__.py
 
-"""
-WhatsApp Service Package for Mahjoub Online
--------------------------------------------
-تغليف حزمة خدمة الواتساب وتوفير الوصول المباشر للـ Blueprint والمكونات الرئيسية.
-"""
-
-from apps.whatsapp_service.routes import whatsapp_bp
-
-__all__ = ['whatsapp_bp']
