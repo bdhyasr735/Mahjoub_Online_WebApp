7882ecbd99c7f03344ff0b81a7cd5da1deede9e1
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-08-29 14:59:57 +0300

    Update __init__.py

--- apps/suppliers_auth_portal/__init__.py
+++ apps/suppliers_auth_portal/__init__.py
@@ -1,10 +1,12 @@
 # coding: utf-8
+# apps/suppliers_auth_portal/__init__.py
+
 from flask import Blueprint
 
 suppliers_auth_bp = Blueprint(
     'suppliers_auth_bp',
     __name__,
-    url_prefix='/suppliers',
+    url_prefix='',
     template_folder='templates',
     static_folder='static'
 )
