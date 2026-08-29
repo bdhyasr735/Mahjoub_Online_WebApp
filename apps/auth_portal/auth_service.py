01c95fdbfea7b2edec5239f182704a858383276c
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-08-29 11:48:20 +0300

    Update auth_service.py

--- apps/auth_portal/auth_service.py
+++ apps/auth_portal/auth_service.py
@@ -1,9 +1,1 @@
-# coding: utf-8
-# 📂 apps/auth_portal/auth_service.py - تم التعطيل (Disabled)
 
-class AdminAuthService:
-    @staticmethod
-    def initiate_login(phone, otp_code):
-        # تم تعطيل هذه الخدمة نهائياً لصالح الولوج المباشر
-        print("INFO: AdminAuthService is disabled. Skipping OTP delivery.")
-        return True
