5c6c0686485629e08e99cf9bbe643226a4b63114
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-08-30 11:17:11 +0300

    Add routes.py for suppliers_auth_portal

---
+++ apps/suppliers_auth_portal/routes.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# 📂 apps/suppliers_auth_portal/routes.py
+
+from apps.suppliers_auth_portal import suppliers_bp
+from apps.suppliers_auth_portal import auth_login, auth_register, auth_recovery
