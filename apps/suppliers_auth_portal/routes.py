f7373bf34f95c4a04ecd98b4f1011e30be69db28
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-08-30 12:22:28 +0300

    Update routes.py

--- apps/suppliers_auth_portal/routes.py
+++ apps/suppliers_auth_portal/routes.py
@@ -1,5 +1,13 @@
 # -*- coding: utf-8 -*-
 # 📂 apps/suppliers_auth_portal/routes.py
 
+from flask import render_template, request
 from apps.suppliers_auth_portal import suppliers_bp
-from apps.suppliers_auth_portal import auth_login, auth_register, auth_recovery
+from apps.suppliers_auth_portal.auth_register import register_supplier_logic
+
+@suppliers_bp.route('/register', methods=['GET', 'POST'])
+def register():
+    """مسار عرض نموذج تسجيل المورد الجديد أو معالجة طلب التسجيل"""
+    if request.method == 'POST':
+        return register_supplier_logic()
+    return render_template('suppliers_auth_portal/register.html')
