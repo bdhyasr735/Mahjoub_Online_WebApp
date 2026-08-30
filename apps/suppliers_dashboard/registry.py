8d79ea1b0f5129d56d371a7750537fe83f749d61
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-08-30 23:24:26 +0300

    Update registry.py

--- apps/suppliers_dashboard/registry.py
+++ apps/suppliers_dashboard/registry.py
@@ -6,24 +6,13 @@ from apps.suppliers_dashboard.routes import suppliers_dashboard_bp
 MODULE_NAME = "لوحة تحكم الموردين"
 DISPLAY_NAME = "لوحة تحكم الموردين"
 MODULE_ICON = "fa-chart-pie"
-SHOW_IN_SUPPLIER = True
-
-NAV_ITEMS = [
-    {
-        'title': 'لوحة التحكم',
-        'endpoint': 'suppliers_dashboard.index'
-    }
-]
-
-LINKS = {
-    'suppliers_dashboard.index': 'لوحة تحكم الموردين'
-}
+IS_LAYOUT_CONTAINER = True
 
 def register_module(app):
-    """تسجيل موديول لوحة تحكم الموردين ديناميكياً"""
+    """تسجيل الإطار الهيكلي لوحة تحكم الموردين ديناميكياً"""
     if suppliers_dashboard_bp.name not in app.blueprints:
         app.register_blueprint(suppliers_dashboard_bp)
-        print(f"✅ [Registry Supplier]: تم تسجيل موديول لوحة تحكم الموردين بنجاح.")
+        print(f"✅ [Registry Supplier Layout]: تم تسجيل هيكل لوحة تحكم الموردين بنجاح.")
 
 def init_app(app):
     pass
