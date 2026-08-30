dacc2ceac9ed9f37f513c5b1e7a78b6d66506ac8
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-08-30 20:17:47 +0300

    Update __init__.py

--- apps/suppliers_auth_portal/__init__.py
+++ apps/suppliers_auth_portal/__init__.py
@@ -1,15 +1,5 @@
 # -*- coding: utf-8 -*-
-# 📂 apps/suppliers_auth_portal/__init__.py
+# apps/suppliers_auth_portal/__init__.py
 
-from flask import Blueprint
-
-suppliers_bp = Blueprint(
-    'suppliers_bp',
-    __name__,
-    template_folder='templates',
-    static_folder='static'
-)
-
-def init_app(app):
-    """تهيئة موديول بوابة الموردين."""
-    pass
+# استيراد الـ Blueprint مباشرة من الملف الموحد
+from apps.suppliers_auth_portal.unified_portal import suppliers_auth_bp
