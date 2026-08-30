d46422ab910f94b3966c3481945b701d7f188c27
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-08-30 12:25:50 +0300

    Update routes.py

--- apps/suppliers_auth_portal/routes.py
+++ apps/suppliers_auth_portal/routes.py
@@ -4,6 +4,8 @@
 from flask import render_template, request
 from apps.suppliers_auth_portal import suppliers_bp
 from apps.suppliers_auth_portal.auth_register import register_supplier_logic
+from apps.suppliers_auth_portal.auth_login import login_supplier_logic
+from apps.suppliers_auth_portal.auth_recovery import request_otp_logic, reset_password_logic
 
 @suppliers_bp.route('/register', methods=['GET', 'POST'])
 def register():
@@ -11,3 +13,25 @@ def register():
     if request.method == 'POST':
         return register_supplier_logic()
     return render_template('suppliers_auth_portal/register.html')
+
+@suppliers_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    """مسار تسجيل الدخول للموردين"""
+    if request.method == 'POST':
+        return login_supplier_logic()
+    return render_template('suppliers_auth_portal/login.html')
+
+@suppliers_bp.route('/forgot-password', methods=['GET'])
+def forgot_password_page():
+    """عرض صفحة استعادة كلمة المرور ذات المرحلتين"""
+    return render_template('suppliers_auth_portal/forgot_password.html')
+
+@suppliers_bp.route('/forgot-password/request-otp', methods=['POST'])
+def request_otp():
+    """معالجة طلب وإرسال رمز التحقق OTP"""
+    return request_otp_logic()
+
+@suppliers_bp.route('/reset-password', methods=['POST'])
+def reset_password():
+    """معالجة التحقق من الرمز وتحديث كلمة المرور"""
+    return reset_password_logic()
