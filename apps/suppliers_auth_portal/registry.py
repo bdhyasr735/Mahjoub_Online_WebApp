94998a7dc6b5eedf32c4b337e0af33a953767b4b
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-08-30 10:32:22 +0300

    Delete apps/suppliers_auth_portal/registry.py

--- apps/suppliers_auth_portal/registry.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# 📂 apps/suppliers_auth_portal/registry.py
-
-# بوابة المصادقة والتسجيل المستقلة (لا تتطلب قوائم جانبية)
-NAV_ITEMS = []
-
-MODULE_NAME = "بوابة المصادقة والتسجيل للموردين"
-MODULE_ICON = "fa-sign-in-alt"
-SHOW_IN_SUPPLIER = False
