7c72788dd6c468143e7ec17a00016726ce164214
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-08-30 14:25:29 +0300

    Update __init__.py

--- apps/suppliers_auth_portal/__init__.py
+++ apps/suppliers_auth_portal/__init__.py
@@ -1,8 +1,17 @@
-# apps/suppliers_auth_portal/__init__.py
+# -*- coding: utf-8 -*-
+# 📂 apps/suppliers_auth_portal/__init__.py
+
+from flask import Blueprint  # ✅ تأكد من وجود هذا السطر
 
 bp = Blueprint(
     'suppliers_auth_portal',
     __name__,
-    template_folder='templates/suppliers_auth_portal',  # ✅ المسار الصحيح
+    template_folder='templates/suppliers_auth_portal',
     url_prefix='/supplier'
 )
+
+from . import auth_login, auth_register, auth_recovery, seo_service
+
+bp.register_blueprint(auth_login.bp)
+bp.register_blueprint(auth_register.bp)
+bp.register_blueprint(auth_recovery.bp)
