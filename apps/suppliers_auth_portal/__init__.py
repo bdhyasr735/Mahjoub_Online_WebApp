4db32f46841abf37fcd8e613e936be3abb4d8c80
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-08-30 09:22:17 +0300

    Update __init__.py

--- apps/suppliers_auth_portal/__init__.py
+++ apps/suppliers_auth_portal/__init__.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
+# 📂 apps/suppliers_auth_portal/__init__.py
+
 from flask import Blueprint
 
 suppliers_bp = Blueprint(
-    'suppliers_auth',  # ✅ الاسم الصحيح
+    'suppliers_bp',
     __name__,
     template_folder='templates',
     static_folder='static'
 )
 
-from apps.suppliers_auth_portal import routes
+# استيراد الملفات المقسمة لتسجيل المسارات (Routes) تلقائياً داخل الـ Blueprint
+from apps.suppliers_auth_portal import auth_login, auth_register, auth_recovery
