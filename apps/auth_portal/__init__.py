acf5f88834f7a87f6f59cece34d3045d3f121aba
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-08-31 13:23:47 +0300

    Update __init__.py

--- apps/auth_portal/__init__.py
+++ apps/auth_portal/__init__.py
@@ -1,1 +1,15 @@
+# -*- coding: utf-8 -*-
+# 📂 apps/auth_portal/__init__.py
 
+from flask import Blueprint
+
+# تعريف البلايبوينت الخاصة ببوابة المصادقة الإدارية
+auth_bp = Blueprint(
+    'auth_bp',
+    __name__,
+    template_folder='templates',
+    static_folder='static'
+)
+
+# استيراد المسارات لضمان تسجيلها داخل البلايبوينت
+from apps.auth_portal import routes
