6f3c61c56e3506ea83b5c6081d314fe0d24780b8
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-08-31 11:39:30 +0300

    Update __init__.py

--- apps/auth_portal/__init__.py
+++ apps/auth_portal/__init__.py
@@ -1,12 +1,1 @@
-# -*- coding: utf-8 -*-
-# apps/auth_portal/__init__.py
 
-"""
-حزمة بوابة المصادقة السيادية الإدارية (Auth Portal Package)
-لمنصة محجوب أونلاين
-"""
-
-from apps.auth_portal.routes import auth_portal_bp
-from apps.auth_portal.registry import auth_registry, AuthPortalRegistry
-
-__all__ = ['auth_portal_bp', 'auth_registry', 'AuthPortalRegistry']
