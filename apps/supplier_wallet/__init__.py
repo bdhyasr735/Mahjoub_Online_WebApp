ad6514a84d0cff20620e6f4c9415c158d44c19c6
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-09-01 13:24:53 +0300

    Update __init__.py

--- apps/supplier_wallet/__init__.py
+++ apps/supplier_wallet/__init__.py
@@ -1,18 +1,14 @@
 # -*- coding: utf-8 -*-
 """
 📂 apps/supplier_wallet/__init__.py
-حزمة موديول محفظة المورد (Supplier Wallet Package)
 """
-
-from apps.supplier_wallet.registry import supplier_wallet_bp, MODULE_NAME, ICON, LINKS, MENU_ITEMS
-
-# استيراد المسارات لضمان تسجيل الـ Routes المرتبطة بالـ Blueprint
+from apps.supplier_wallet.registry import supplier_wallet_bp, MODULE_NAME, MODULE_ICON, LINKS, register_module
 from apps.supplier_wallet import routes
 
 __all__ = [
     'supplier_wallet_bp',
     'MODULE_NAME',
-    'ICON',
+    'MODULE_ICON',
     'LINKS',
-    'MENU_ITEMS'
+    'register_module'
 ]
