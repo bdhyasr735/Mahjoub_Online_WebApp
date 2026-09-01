666a4d802bd2d63b42f9c72e3e25d1f4fd472899
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-09-01 12:19:43 +0300

    Refactor supplier_wallet init for cleaner structure
    Removed unused imports and added module exports.

--- apps/supplier_wallet/__init__.py
+++ apps/supplier_wallet/__init__.py
@@ -1,37 +1,10 @@
 # -*- coding: utf-8 -*-
-# 📂 apps/supplier_wallet/__init__.py
+"""
+📂 apps/supplier_wallet/__init__.py
+تهيئة حزمة محفظة المورد وتصدير المكونات الأساسية لنظام التسجيل الديناميكي
+"""
 
-from decimal import Decimal
-from flask import Blueprint, render_template, request, redirect, url_for, flash
-from flask_login import login_required
-from apps.extensions import db
+from apps.supplier_wallet.registry import register_module, MODULE_NAME, ICON, MENU_ITEMS
+from apps.supplier_wallet.routes import wallet_bp
 
-# استيراد آمن لنماذج قاعدة بيانات المحفظة لمنع أي خطأ استيراد
-try:
-    from apps.models.wallet_db import SupplierWallet
-except ImportError:
-    SupplierWallet = None
-
-try:
-    from apps.models.wallet_db import WalletTransaction
-except ImportError:
-    WalletTransaction = None
-
-from apps.supplier_wallet.services.wallet_service import WalletService
-from apps.supplier_wallet.services.notification_service import NotificationService
-from apps.supplier_wallet.utils import get_current_supplier_id
-
-# استيراد البيانات مباشرة من ملفات الداتا
-from apps.data.yemen_banks import YEMEN_BANKS
-from apps.data.financial_companies import FINANCIAL_COMPANIES
-
-# توحيد تعريف البلوبرنت ليتطابق مع اسم الـ endpoints والـ registry
-supplier_wallet_bp = Blueprint(
-    'supplier_wallet',
-    __name__,
-    template_folder='templates',
-    static_folder='static'
-)
-
-# استيراد المسارات لضمان تسجيلها عند تحميل البلوبرنت
-from apps.supplier_wallet import routes
+__all__ = ['register_module', 'wallet_bp', 'MODULE_NAME', 'ICON', 'MENU_ITEMS']
