7df34ed93494c2f37cb743bb228984db3857ae30
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-09-01 12:48:02 +0300

    Update registry.py

--- apps/supplier_wallet/registry.py
+++ apps/supplier_wallet/registry.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
-from flask import url_for
 
 MODULE_NAME = "الإدارة المالية"
 ICON = "fas fa-wallet"
 
-# تعريف الروابط بحيث تتطابق مع أسماء الدوال في الـ routes.py
+# تعريف القائمة كقاموس مباشر يربط الـ endpoint بالعنوان الظاهر
 MENU_ITEMS = {
     'supplier_wallet.wallet_dashboard_redirect': 'حركة المحفظة',
     'supplier_wallet.withdraw': 'سحب الرصيد'
