d57a6772591c79bf6854af8ccf3e5414497b9008
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-09-02 14:40:41 +0300

    Update routes.py

--- apps/suppliers_auth_portal/routes.py
+++ apps/suppliers_auth_portal/routes.py
@@ -9,7 +9,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required, current_user
 
 from apps.extensions import db
-from apps.models.suppliers_db import Supplier  # نموذج جدول الموردين المصحح
+from apps.models.supplier_db import Supplier  # تم تعديل اسم الملف ليكون بدون حرف s
 from apps.models.wallet import SupplierWallet  # نموذج محفظة المورد المالية
 from apps.suppliers_auth_portal.otp_service import SupplierOTPService
 
