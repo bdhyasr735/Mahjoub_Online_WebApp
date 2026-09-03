08bbb3acaf507b6e5ad432e8285c41e60a8f9877
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-09-03 22:44:56 +0300

    Update registry.py

--- apps/suppliers_auth_portal/registry.py
+++ apps/suppliers_auth_portal/registry.py
@@ -1,16 +1,10 @@
 # -*- coding: utf-8 -*-
-"""
-ملف التسجيل لبوابة مصادقة الموردين
-يستخدم لتسجيل البوابة وملحقاتها في التطبيق الرئيسي
-"""
-
 import logging
 from apps.suppliers_auth_portal.routes import suppliers_auth_bp
 
 logger = logging.getLogger(__name__)
 
 def register_module(app):
-    """تسجيل بوابة الموردين في تطبيق الفلاسك الرئيسي"""
     try:
         if suppliers_auth_bp.name not in app.blueprints:
             app.register_blueprint(suppliers_auth_bp, url_prefix='/supplier')
