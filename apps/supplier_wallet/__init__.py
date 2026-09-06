0d5cad56ef7dee25d89c4167ca64c34a8dee157f
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-09-06 10:46:01 +0300

    Update __init__.py

--- apps/supplier_wallet/__init__.py
+++ apps/supplier_wallet/__init__.py
@@ -1,3 +1,13 @@
-from .registry import supplier_wallet_bp, register_supplier_wallet_app
+# -*- coding: utf-8 -*-
+# 📂 apps/supplier_wallet/__init__.py
 
-__all__ = ['supplier_wallet_bp', 'register_supplier_wallet_app']
+from apps.supplier_wallet.registry import supplier_wallet_bp, init_app, MODULE_NAME, ICON, SHOW_IN_SUPPLIER, LINKS
+
+__all__ = [
+    'supplier_wallet_bp',
+    'init_app',
+    'MODULE_NAME',
+    'ICON',
+    'SHOW_IN_SUPPLIER',
+    'LINKS'
+]
