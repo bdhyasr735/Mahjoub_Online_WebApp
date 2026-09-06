420aa70b671c779dddb52b57a46d2895c8da7e30
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-09-06 10:20:50 +0300

    Update __init__.py

--- apps/suppliers_dashboard/__init__.py
+++ apps/suppliers_dashboard/__init__.py
@@ -1,16 +1,7 @@
-# apps/suppliers_dashboard/__init__.py
-from flask import Blueprint
+# -*- coding: utf-8 -*-
+# 📂 apps/suppliers_dashboard/__init__.py
 
-suppliers_dashboard_bp = Blueprint(
-    'suppliers_dashboard',
-    __name__,
-    template_folder='templates',
-    static_folder='static'
-)
+from apps.suppliers_dashboard.registry import suppliers_dashboard_bp, init_app
 
-def register_module(app):
-    """دالة التسجيل الديناميكي لتتوافق مع نظام المحمل العام في محجوب أونلاين"""
-    app.register_blueprint(suppliers_dashboard_bp, url_prefix='/suppliers')
-    # يمكنك تعديل الـ url_prefix حسب مسار التطبيق لديك
-
-from apps.suppliers_dashboard import routes
+# تصدير الـ Blueprint ودالة التسجيل لتكون جاهزة للاستدعاء في التطبيق الرئيسي
+__all__ = ['suppliers_dashboard_bp', 'init_app']
