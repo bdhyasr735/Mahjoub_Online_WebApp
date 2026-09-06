cff7d248676c1264038370647f2781f3b6892b7c
علي محمد محمد علي محجوب <alimahgoob848@gmail.com>
2026-09-06 11:22:00 +0300

    Update __init__.py

--- apps/supplier_wallet/services/__init__.py
+++ apps/supplier_wallet/services/__init__.py
@@ -1,8 +1,10 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
 """
 📂 apps/supplier_wallet/services/__init__.py
-حزمة خدمات موديول محفظة المورد (Supplier Wallet Services Package)
+حزمة خدمات المحفظة والعمليات المالية للموردين
 """
 
 from apps.supplier_wallet.services.wallet_service import WalletService
 from apps.supplier_wallet.services.notification_service import NotificationService
+
+__all__ = ['WalletService', 'NotificationService']
